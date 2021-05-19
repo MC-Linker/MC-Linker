b287da5c8382f24e4579c3dac4aea42ac183a818
Lianecx <emilian.klefe@web.de>
2021-05-19 11:43:07 +0200

    Updated PingBot help

--- Discord.jsBot/main.js
+++ Discord.jsBot/main.js
@@ -25,13 +25,15 @@ client.on('guildMemberAdd', async member => {
 client.on('message', (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    if(message.mentions.user.first().tag === "SMP Bot#7784") {
+            console.log(message.member.user.tag + "pinged Bot")
+            message.channel.send("Prefix = ^ \n^help for detailed description of commands.")
+    }
+
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
 
-        if(message.mentions.user.first().tag === "SMP Bot#7784") {
-            console.log(message.member.user.tag + "pinged Bot")
-            message.channel.send("Prefix = ^ \n^help for detailed description of commands.")
-        }
+        
 
         if(command === 'pingchain') {
             
