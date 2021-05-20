55fe801a4efe05b65bc809589c4b710f69958a76
Lianecx <emilian.klefe@web.de>
2021-05-20 13:15:42 +0200

    Update main.js

--- Discord.jsBot/main.js
+++ Discord.jsBot/main.js
@@ -92,7 +92,6 @@ client.on('message', (message) => {
                                         if(data.disable === 'disabled') {
                                             console.log('Category disabled.')
                                             message.channel.send('Category disabled!')
-                                            return;
                                         } else if(data.disable === 'enabled') {
                                             console.log('Category enabled.')
                                         }
@@ -111,7 +110,6 @@ client.on('message', (message) => {
                                         if(data.disable === 'disabled') {
                                             console.log('Object disabled.')
                                             message.channel.send('Object disabled!')
-                                            return;
                                         } else if(data.disable === 'enabled') {
                                             console.log('Object enabled.')
                                         } 
