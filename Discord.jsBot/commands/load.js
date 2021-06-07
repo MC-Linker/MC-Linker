2a963e1d5656a364a48ed1faf5a72f80bace6d68
Lianecx <emilian.klefe@web.de>
2021-06-07 14:14:09 +0200

    Update load.js

--- Discord.jsBot/commands/load.js
+++ Discord.jsBot/commands/load.js
@@ -3,12 +3,13 @@ module.exports = {
 	execute(message, args) {
 
 		const Discord = require('discord.js');
-		const Canvas = require('canvas');
+		//const Canvas = require('canvas');
 
 		const base = args[0]
 		const baseWidth = base.naturalWidth
 		const baseHeight = base.naturalHeight
 		console.log(baseWidth + 'x' + baseHeight)
+		message.channel.send(baseWidth + 'x' + baseHeight)
 
 		//const loading = Canvas.createCanvas(baseWidth * 2, baseHeight * 2);
 		//const context = loading.getContext('2d');
