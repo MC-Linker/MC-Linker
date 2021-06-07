8149d2463da2d367bd3df4789ea4e9abb4aeb15e
Lianecx <emilian.klefe@web.de>
2021-06-07 15:41:05 +0200

    Deleted ^load

--- Discord.jsBot/main.js
+++ Discord.jsBot/main.js
@@ -122,10 +122,6 @@ client.on('message', (message) => {
 
             client.commands.get('advancements').execute(message, args);
 
-        } else if (command === 'load') {
-
-            client.commands.get('load').execute(message, args);
-
         }
     })
 client.login(process.env.token)
