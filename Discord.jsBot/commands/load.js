66353e39646205bcabeeae9aed35939907804b0c
Lianecx <emilian.klefe@web.de>
2021-06-07 14:57:04 +0200

    Updated ^load

--- Discord.jsBot/commands/load.js
+++ Discord.jsBot/commands/load.js
@@ -5,16 +5,18 @@ module.exports = {
 		const Discord = require('discord.js');
 		//const Canvas = require('canvas');
 
-		var gm = require('gm');
-		var request = require('request');
-		var url = (args[0]);
-		
-		var stream = request(url);
-		gm(stream, './img.jpg').size({ bufferStream: true }, function (err, size) {
-			if (err) { throw err; }
-			console.log(size);
-		})
+		const probe = require('probe-image-size');
 
+		let width;
+		let height;
+		async function getSize(url) {
+			let size = await probe(url);
+			width = size.width;
+			height = size.height;
+		}
+		getSize(args[0]);
+		
+		console.log(height + 'x' + width)
 		//const loading = Canvas.createCanvas(baseWidth * 2, baseHeight * 2);
 		//const context = loading.getContext('2d');
 	}
