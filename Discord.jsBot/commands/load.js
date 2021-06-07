7ba2a5faccd7086e09e5cf26e799dc1911a2b60f
Lianecx <emilian.klefe@web.de>
2021-06-07 14:22:09 +0200

    Update load.js

--- Discord.jsBot/commands/load.js
+++ Discord.jsBot/commands/load.js
@@ -5,12 +5,15 @@ module.exports = {
 		const Discord = require('discord.js');
 		//const Canvas = require('canvas');
 
-		const base = new Image()
-		const baseWidth = base.naturalWidth
-		const baseHeight = base.naturalHeight
-		console.log(baseWidth + 'x' + baseHeight)
-		message.channel.send(baseWidth + 'x' + baseHeight)
-		base.src = (args[0])
+		function getMeta(url){   
+			var img = new Image();
+			img.addEventListener("load", function(){
+				console.log(this.naturalWidth +' '+ this.naturalHeight);
+			});
+			img.src = url;
+		}
+		
+		getMeta(args[0])
 
 		//const loading = Canvas.createCanvas(baseWidth * 2, baseHeight * 2);
 		//const context = loading.getContext('2d');
