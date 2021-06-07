d0217450a04911e5546b4e41742a7e903cc558d7
Lianecx <emilian.klefe@web.de>
2021-06-07 15:20:57 +0200

    Updated ^load

--- Discord.jsBot/commands/load.js
+++ Discord.jsBot/commands/load.js
@@ -4,18 +4,33 @@ module.exports = {
 
 		const Discord = require('discord.js');
 		//const Canvas = require('canvas');
+		const fs = require('fs');
+		const request = require('request');
+		const imgsize = require('image-size');
 
-		const probe = require('probe-image-size');
-
-		let width;
 		let height;
-		async function getSize() {
-			let size = await probe('https://cdn.discordapp.com/attachments/844493685244297226/850708455156482048/cooked_mutton.png');
-			width = size.width;
-			height = size.height;
+		let width;
+	
+		function getImgSize() {
+			var downloadImg = function(url, filename, callback){
+			request.head(url, function(err, res, body){
+				console.log('content-type:', res.headers['content-type']);
+				console.log('content-length:', res.headers['content-name']);
+			
+				request(url).pipe(fs.createWriteStream(filename)).on('close', callback);
+			});
+			};
+			
+			downloadImg(args[0], './images/loadImage.png', function(){
+				console.log('Downloaded image.');
+			});
+
+			const dimensions = imgsize('./images/loadImage.png');
+			width = dimensions.width;
+			height = dimensions.height;
 		}
-		getSize();
 		
+
 		console.log(height + 'x' + width)
 		//const loading = Canvas.createCanvas(baseWidth * 2, baseHeight * 2);
 		//const context = loading.getContext('2d');
