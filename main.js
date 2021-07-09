6229e013b06729b99130dd10f6860b0bf8ca7302
Lianecx <emilian.klefe@web.de>
2021-07-09 15:38:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log('Loading...')
 
 //const Discord = require('discord.js')
 const prefix = "^";
-const { prefix, token } = require('../config.json');
+//const { prefix, token } = require('../config.json');
 const client = new Discord.Client()
 const fs = require('fs');
 const disbut = require('discord-buttons');
