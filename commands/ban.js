62b6d9a8f9738c2915a028e2e56d19440832cf27
Lianecx <emilian.klefe@web.de>
2021-07-19 22:29:05 +0200

    ^Finished ban command: Needs a serverrestart, Optimized logs, guildJoin: Now shows memberCount, Added getUserName to utils and ^connect

--- commands/ban.js
+++ commands/ban.js
@@ -7,43 +7,83 @@ module.exports = {
     async execute(message, args) {
 		const utils = require('../utils');
 		const ftp = require('../ftpConnect');
-		
-		let reason;
-		for(let i = 1; i<args.length; i++) {
-			reason += args[i];
-		}
-		if(!args[1]) reason = 'No specified reason.';
-
-		const taggedUser = (args[0]);
-        if(!taggedUser) {
+		const fs = require('fs');
+		const taggedUser = args.shift();
+		if(!taggedUser) {
             console.log(message.member.user.tag + ' executed ^ban without user in ' + message.guild.name);
             message.reply('Please specify the player you want to ban.');
             return;
         }
+
+		let reason = '';
+		if(!args[0]) reason = 'Banned by an operator.';
+		else reason = args.join(' ');
+
 		let taggedName;
+		let userName;
 		if(!message.mentions.users.size) {
-            taggedName = (args[0]);
+			userName = taggedUser;
+            taggedName = taggedUser;
         } else {
+			userName = await utils.getUserName(message.mentions.users.first().id, message);
+			if(userName === undefined) return;
             taggedName = message.mentions.users.first().tag;
         }
-		console.log(message.member.user.tag + ' executed ^ban in ' + message.guild.name)
+		console.log(message.member.user.tag + ' executed ^ban with taggedUser: ' + taggedName + ' in ' + message.guild.name)
 
-		if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+		if (!message.member.hasPermission("BAN_MEMBERS")) {
 			message.reply(':warning: ' + "This command can only be used with `Ban member` permission!");
             console.log(message.member.user.tag + ' executed ^ban without banperm in ' + message.guild.name);
             return;
 		}
 		const uuidv4 = await utils.getUUIDv4(taggedUser, message);
-        if(uuidv4 === undefined) {
-            return;
-        }
+        if(uuidv4 === undefined) return;
+
 		let worldPath = await utils.getWorldPath(message);
-		if(worldPath === undefined) {
-            return;
-        }
+		if(worldPath === undefined) return;
+
 		worldPath = worldPath.split('/');
 		let path = worldPath[0];
 		if(path === '') path = worldPath[1];
-		await ftp.get(`${path}/banned-players.json`, `./ban/${message.guild.id}.json`, message);
+		await ftp.get(`${path}/banned-players.json`, `./bans/${message.guild.id}.json`, message);
+
+		const banReplace = {
+			"uuid": uuidv4,
+			"name": userName,
+			"created": new Date(),
+			"source": "Smp_Minecraft_Bot",
+			"expires": "forever",
+			"reason": reason
+		}
+
+		fs.readFile(`./bans/${message.guild.id}.json`, 'utf8', (err, banJson) => {
+			if (err) {
+				console.log('Error reading ban file from disk ', err);
+				message.reply('<:Error:849215023264169985> ' + 'Could not read ban file.');
+                return;
+			}
+			let banData = JSON.parse(banJson);
+			for(let i = 0; i<banData.length; i++) {
+				if(banData[i].uuid === uuidv4) {
+					console.log('Player is already banned.');
+					message.reply(':warning: Player [**' + taggedName + '**] is already banned.');
+					return;
+				}
+			}
+			banData.splice(0, 0, banReplace);
+
+			const banString = JSON.stringify(banData, null, 2);
+			fs.writeFile(`./bans/${message.guild.id}.json`, banString, async (err) => {
+				if(err) {
+					console.log('Error writing banFile');
+					message.reply('<:Error:849215023264169985> Error trying to ban player.');
+					return;
+				}
+				await ftp.put(`./bans/${message.guild.id}.json`, `${path}/banned-players.json`, message);
+
+				console.log('Succesfully wrote and put banFile on server.');
+				message.reply('<:Checkmark:849224496232660992> Succesfully banned player [**' + taggedName + '**] from the minecraft server.');	
+			})
+		})
 	}
 }
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ client.once('ready', () => {
 })
 
 client.on("guildCreate", guild => {
-    console.log("Joined a new guild: " + guild.name + '\nBot is now on ' + client.guilds.cache.size + ' server!');
+    console.log("Joined a new guild: " + guild.name + ': ' + guild.memberCount + ' members.\nBot is now on ' + client.guilds.cache.size + ' server!');
 })
 client.on("guildDelete", guild => {
     console.log("Left a guild: " + guild.name + '\nBot is now on ' + client.guilds.cache.size + ' server!');
@@ -62,7 +62,7 @@ client.on('message', (message) => {
             try {
                 command = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
                 if (command === undefined) {console.log(message.member.user.tag + ' executed non-existent help command ' + commandName + ' in ' + message.guild.name); return;}
-                console.log(message.member.user.tag + ' executed ^help ' + command.name);
+                console.log(message.member.user.tag + ' executed ^help ' + command.name + ' in ' + message.guild.name);
 
                 helpEmbed = new Discord.MessageEmbed()
                     .setTitle('Help Menu')
@@ -88,7 +88,6 @@ client.on('message', (message) => {
 
             const disabled = fs.existsSync('./disable/command/' + message.guild.id + '_' + command.name);
             if (disabled === false) {
-                console.log('disableCommandFile doesnt exist. Command not disabled.');
                 message.channel.send({embed: helpEmbed, button: disableButton});
             } else if (disabled === true) {
                 helpEmbed.setDescription('You can find helpful information here. \n ```diff\n- [COMMAND DISABLED]```');
