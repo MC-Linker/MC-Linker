886d4b2f1f60f251df5085d46e2b718a89e2c6fd
Lianecx <84291452+Lianecx@users.noreply.github.com>
2021-07-21 16:00:32 +0200

    Added GItInit for properties folder

--- rcon.js
+++ rcon.js
@@ -1,11 +1,19 @@
 module.exports = {
 	execute: async function (ip, port, pass, command, message) {
 		const rcon = require('rcon');
+		const Discord = require('discord.js');
 
 		const rconCLient = new rcon(ip, port, pass, {
 			tcp: true,
 			challenge: false
 		});
+
+		let respEmbed;
+		if (command.startsWith('ban')) {
+			respEmbed = new Discord.MessageEmbed().setTitle('Ban player').setColor('ORANGE');
+		} else if (command.startsWith('pardon')) {
+			respEmbed = new Discord.MessageEmbed().setTitle('Unban player').setColor('BLUE');
+		}
 		
 		rconCLient.connect();
 
@@ -16,7 +24,8 @@ module.exports = {
 
 		}).on('response', (response) => {
 			console.log(response);
-			message.reply('<:Checkmark:849224496232660992> ' + response);
+			if(respEmbed !== undefined) {respEmbed.setDescription(response); message.channel.send(respEmbed);}
+			else message.reply('<:Checkmark:849224496232660992> ' + response)
 
 		}).on('error', (err) => {
 			console.log('Error trying to execute Command: ' + command , err);
