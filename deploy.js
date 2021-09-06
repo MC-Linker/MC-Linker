21207f9d3f780de44b31a62a46e59671a47e759f
Lianecx <84291452+Lianecx@users.noreply.github.com>
2021-09-06 10:51:59 +0200

    Fixed slash commands

--- deploy.js
+++ deploy.js
@@ -24,6 +24,11 @@ const rest = new REST({ version: '9' }).setToken(token);
 	try {
 		console.log('Started refreshing application (/) commands.');
 
+		/*await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);*/
+
 		await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
