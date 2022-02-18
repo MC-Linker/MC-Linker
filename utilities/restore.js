8181dc76dc5039a729af5f13c9415efc8b9fe06a
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-02-18 15:39:03 +0100

    Added improved restore.js and renamed /temp to /utilities

---
+++ utilities/restore.js
@@ -0,0 +1,43 @@
+//Restores the connection.json from all connection files.
+const fs = require('fs');
+restoreConnections();
+
+function restoreConnections() {
+    //Read guild connection folders
+    const serverFolders = fs.readdirSync('./serverdata/connections');
+    console.log('Read server connection folders...');
+
+    //If connections.json already exists, return
+    if(serverFolders.includes('connections.json')) return console.log('connections.json already exists. Please delete it if you still want to restore.');
+
+    const connections = [];
+    //Loop over guild connections
+    for (const serverFolder of serverFolders) {
+        console.group(`Restoring ${serverFolder}...`);
+
+        let serverConnection;
+        try {
+            serverConnection = JSON.parse(fs.readFileSync(`./serverdata/connections/${serverFolder}/connection.json`, 'utf-8'));
+        } catch(err) {
+            console.log('Couldn\'t read connection file...', err);
+            continue;
+        }
+
+        const connJson = {
+            "guildId": serverConnection.guild,
+            "hash": serverConnection.hash,
+            "chat": serverConnection.chat,
+            "ip": serverConnection.ip
+        }
+        if(serverConnection.channel) connJson.channelId = serverConnection.channel;
+
+        connections.push(connJson);
+        console.log('Pushed connection...');
+        console.groupEnd();
+    }
+
+    //Write new connections.json
+    fs.writeFileSync('./serverdata/connections/connections.json', JSON.stringify(connections, null, 2), 'utf-8');
+}
+
+module.exports = { restoreConnections };
