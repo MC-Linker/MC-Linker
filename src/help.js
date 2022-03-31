3561870805d449924be023e9625090bd64299085
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-03-31 15:49:36 +0200

    Updated help.js with new command template

--- src/help.js
+++ src/help.js
@@ -1,73 +1,63 @@
 const fs = require('fs');
-const { SlashCommandBuilder } = require('@discordjs/builders');
 const { discordLink } = require('../config.json');
 const { keys, getEmbedBuilder, ph, addPh, getComponentBuilder} = require('../api/messages');
 const settings = require('../api/settings');
 
+async function execute(message, args) {
+    if(!args[0]) {
+        message.respond(keys.commands.help.success.no_args, { "invite_link": discordLink });
+    } else {
+        const commandName = args[0].toLowerCase();
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Detailed Description of every command.')
-        .addStringOption(option =>
-            option.setName('command')
-                .setDescription('Set the command or category of which you want to get infos.')
-                .setRequired(false)
-        ),
-    async execute(message, args) {
-        if(!args[0]) {
-            message.respond(keys.commands.help.success.no_args, { "invite_link": discordLink });
-        } else {
-            const commandName = args[0].toLowerCase();
+        let command = keys.data[commandName];
+        if (!command) {
+            fs.readdir(`./commands/${commandName}`, (err, commands) => {
+                if(err) {
+                    message.respond(keys.commands.help.warnings.command_does_not_exist, { "command_name": commandName });
+                }
 
-            let command = keys.data[commandName];
-            if (!command) {
-                fs.readdir(`./commands/${commandName}`, (err, commands) => {
-                    if(err) {
-                        message.respond(keys.commands.help.warnings.command_does_not_exist, { "command_name": commandName });
-                    }
-
-                    commands = commands.filter(command => command.endsWith('.js'));
-                    const helpEmbed = getEmbedBuilder(keys.commands.help.success.base, ph.fromStd(message));
-                    commands.forEach(commandFile => {
-                        commandFile = commandFile.split('.').shift();
-                        command = keys.data[commandFile];
-
-                        helpEmbed.addField(
-                            addPh(keys.commands.help.success.category.fields.command.name, { "command_name": command.name }),
-                            addPh(keys.commands.help.success.category.fields.command.content, { "command_short_description": command.short_description })
-                        );
-                    });
+                commands = commands.filter(command => command.endsWith('.js'));
+                const helpEmbed = getEmbedBuilder(keys.commands.help.success.base, ph.fromStd(message));
+                commands.forEach(commandFile => {
+                    commandFile = commandFile.split('.').shift();
+                    command = keys.data[commandFile];
 
                     helpEmbed.addField(
-                        keys.commands.help.success.category.fields.information.name,
-                        addPh(keys.commands.help.success.category.fields.information.content, { "discord_link": discordLink })
+                        addPh(keys.commands.help.success.category.fields.command.name, { "command_name": command.name }),
+                        addPh(keys.commands.help.success.category.fields.command.content, { "command_short_description": command.short_description })
                     );
-                    message.reply({ embeds: [helpEmbed] });
                 });
-            } else {
-                const helpEmbed = getEmbedBuilder(
-                    keys.commands.help.success.command,
-                    ph.fromStd(message),
-                    { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
-                );
 
-                const disabled = await settings.getDisabled(message.guildId, 'commands');
-                if (!disabled.find(disable => disable === command.name)) {
-                    const disableRow = getComponentBuilder(
-                        keys.commands.help.success.disable_button.components,
-                        { "command_name": command.name }, ph.fromStd(message)
-                    );sss
-                    message.reply({ embeds: [helpEmbed], components: [disableRow] });
-                } else if (disabled) {
-                    const enableRow = getComponentBuilder(
-                        keys.commands.help.success.enable_button.components,
-                        { "command_name": command.name }, ph.fromStd(message)
-                    );
-                    helpEmbed.setDescription(keys.commands.help.success.disabled.description);
-                    message.reply({ embeds: [helpEmbed], components: [enableRow] });
-                }
+                helpEmbed.addField(
+                    keys.commands.help.success.category.fields.information.name,
+                    addPh(keys.commands.help.success.category.fields.information.content, { "discord_link": discordLink })
+                );
+                message.reply({ embeds: [helpEmbed] });
+            });
+        } else {
+            const helpEmbed = getEmbedBuilder(
+                keys.commands.help.success.command,
+                ph.fromStd(message),
+                { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
+            );
+
+            const disabled = await settings.getDisabled(message.guildId, 'commands');
+            if (!disabled.find(disable => disable === command.name)) {
+                const disableRow = getComponentBuilder(
+                    keys.commands.help.success.disable_button.components,
+                    { "command_name": command.name }, ph.fromStd(message)
+                );sss
+                message.reply({ embeds: [helpEmbed], components: [disableRow] });
+            } else if (disabled) {
+                const enableRow = getComponentBuilder(
+                    keys.commands.help.success.enable_button.components,
+                    { "command_name": command.name }, ph.fromStd(message)
+                );
+                helpEmbed.setDescription(keys.commands.help.success.disabled.description);
+                message.reply({ embeds: [helpEmbed], components: [enableRow] });
             }
         }
     }
 }
+
+module.exports = { execute };
