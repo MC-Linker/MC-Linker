80e1dea25847271267e0e38edf5dc4482ca71163
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-04-01 15:43:28 +0200

    Formatted enableButton and disableButton

--- src/disableButton.js
+++ src/disableButton.js
@@ -3,30 +3,32 @@ const settings = require('../api/settings');
 const { keys, ph, getEmbedBuilder, getComponentBuilder} = require('../api/messages');
 
 async function execute(interaction) {
-    if (interaction.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-        const commandName = interaction.customId.split('_').pop();
-
-        //Disable command
-        const disabled = await settings.disable(interaction.guildId, 'commands', commandName);
-        if(disabled) {
-            interaction.respond(keys.buttons.disable.success.response, { "command_name": commandName });
-        } else {
-            interaction.respond(keys.buttons.disable.errors.could_not_disable, { "command_name": commandName });
-        }
-
-        const command = keys.data[commandName];
-
-        const enableRow = getComponentBuilder(keys.commands.help.success.enable_button);
-        const helpEmbed = getEmbedBuilder(
-            keys.commands.help.success.command,
-            ph.fromStd(interaction),
-            { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
-        );
-
-        interaction.message.edit({ embeds: [helpEmbed], components: [enableRow] });
-    } else {
+    if (!interaction.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
         interaction.respond(keys.buttons.disable.warnings.no_permission);
+        return;
     }
+
+    const commandName = interaction.customId.split('_').pop();
+
+    //Disable command
+    const disabled = await settings.disable(interaction.guildId, 'commands', commandName);
+    if(disabled) {
+        interaction.respond(keys.buttons.disable.success.response, { "command_name": commandName });
+    } else {
+        interaction.respond(keys.buttons.disable.errors.could_not_disable, { "command_name": commandName });
+        return;
+    }
+
+    const command = keys.data[commandName];
+
+    const enableRow = getComponentBuilder(keys.commands.help.success.enable_button);
+    const helpEmbed = getEmbedBuilder(
+        keys.commands.help.success.command,
+        ph.fromStd(interaction),
+        { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
+    );
+
+    interaction.message.edit({ embeds: [helpEmbed], components: [enableRow] });
 }
 
 module.exports = { execute };
--- src/enableButton.js
+++ src/enableButton.js
@@ -3,30 +3,32 @@ const settings = require('../api/settings');
 const { keys, ph, getEmbedBuilder, getComponentBuilder} = require('../api/messages');
 
 async function execute(interaction) {
-    if (interaction.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-        const commandName = interaction.customId.split('_').pop();
-
-        const enable = await settings.enable(interaction.guildId, 'commands', commandName);
-        if(enable) {
-            interaction.respond(keys.buttons.enable.success.response, { "command_name": commandName });
-        } else {
-            interaction.respond(keys.buttons.enable.errors.already_enabled, { "command_name": commandName });
-        }
-
-        const command = interaction.client.commands.get(commandName);
-
-        const disableRow = getComponentBuilder(keys.commands.help.success.enable_button, { "command_name": commandName });
-        const helpEmbed = getEmbedBuilder(
-          keys.commands.help.success.command,
-            ph.fromStd(interaction),
-            { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
-        ).setDescription(keys.buttons.enable.success.help.description)
-        .setColor(keys.buttons.enable.success.help.color);
-
-        interaction.message.edit({ embeds: [helpEmbed], components: [disableRow] });
-    } else {
+    if (!interaction.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
         interaction.respond(keys.buttons.enable.warnings.no_permission);
+        return;
     }
+
+    const commandName = interaction.customId.split('_').pop();
+
+    const enable = await settings.enable(interaction.guildId, 'commands', commandName);
+    if(enable) {
+        interaction.respond(keys.buttons.enable.success.response, { "command_name": commandName });
+    } else {
+        interaction.respond(keys.buttons.enable.errors.already_enabled, { "command_name": commandName });
+        return;
+    }
+
+    const command = interaction.client.commands.get(commandName);
+
+    const disableRow = getComponentBuilder(keys.commands.help.success.enable_button, { "command_name": commandName });
+    const helpEmbed = getEmbedBuilder(
+      keys.commands.help.success.command,
+        ph.fromStd(interaction),
+        { "command_name": command.name.cap(), "command_long_description": command.long_description, "command_usage": command.usage, "command_example": command.example }
+    ).setDescription(keys.buttons.enable.success.help.description)
+    .setColor(keys.buttons.enable.success.help.color);
+
+    interaction.message.edit({ embeds: [helpEmbed], components: [disableRow] });
 }
 
 module.exports = { execute };
