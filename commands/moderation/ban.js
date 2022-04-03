c4cd50be52c10516f9f9f16070056d9bf969854e
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-04-03 13:44:55 +0200

    Added keys to ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,23 +1,21 @@
 const utils = require('../../api/utils');
 const plugin = require('../../api/plugin');
 const Discord = require('discord.js');
+const { keys } = require('../../api/messages');
 
 async function execute(message, args) {
 	const username = message.mentions.users.first()?.tag ?? args[0];
 	args.shift();
 	let reason = args[0] ? args.join(' ') : 'Banned by an operator.';
+	const argPlaceholder = { username, reason };
 
 	if (!message.member.permissions.has(Discord.Permissions.FLAGS.BAN_MEMBERS)) {
-		console.log(`${message.member.user.tag} executed /ban without ban permission in ${message.guild.name}`);
-		message.reply(':no_entry: This command can only be used with `Ban member` permission!');
+		message.respond(keys.commands.ban.warnings.no_permission);
 		return;
 	} else if(!username) {
-		console.log(`${message.member.user.tag} executed /ban without username in ${message.guild.name}`);
-		message.reply(':warning: Please specify the player you want to ban.');
+		message.respond(keys.commands.ban.warnings.no_username);
 		return;
 	}
-
-	console.log(`${message.member.user.tag} executed /ban ${username} ${reason} in ${message.guild.name}`);
 
 	const mcUsername = message.mentions.users.first()?.id ? await utils.getUsername(message.mentions.users.first()?.id, message) : username;
 	if(!mcUsername) return;
@@ -25,14 +23,10 @@ async function execute(message, args) {
 	const resp = await plugin.execute(`ban ${mcUsername} ${reason}`, message);
 	if(!resp) return;
 
-	const respEmbed = new Discord.MessageEmbed()
-		.setTitle('Ban Player')
-		.setColor('ORANGE');
 
-	if(resp.startsWith('&c')) respEmbed.setDescription(`Warning trying to ban player [**${username}**]: ${resp.replace('&c', '')}`);
-	else if(resp.startsWith('Could not fetch response message!')) respEmbed.setDescription(`:warning: Successfully executed ban player [**${username}**]: \`${reason}\`\n${resp}`);
-	else respEmbed.setDescription(`<:Checkmark:849224496232660992> Successfully banned player [**${username}**]: \`${reason}\``);
-	message.reply({ embeds: [respEmbed] });
+	if(resp.startsWith('&c')) message.respond(keys.commands.ban.warnings.response_warning, argPlaceholder, { "response": resp });
+	else if(resp.startsWith('Could not fetch response message!')) message.respond(keys.commands.ban.warnings.no_response, argPlaceholder);
+	else message.respond(keys.commands.ban.success, argPlaceholder);
 }
 
 module.exports = { execute };
