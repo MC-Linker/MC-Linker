291ead4991d2f0181400383bb5ed5a659d55c0a9
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-04-04 14:37:15 +0200

    Added keys to chatchannel.js

--- commands/settings/chatchannel.js
+++ commands/settings/chatchannel.js
@@ -2,79 +2,24 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const utils = require('../../api/utils');
 const plugin = require('../../api/plugin');
+const { keys, getEmbedBuilder, ph } = require('../../api/messages');
 
 async function execute(message, args) {
     let channel = message.mentions.channels?.first() ?? args[0];
 
     if(!message.member.permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR)) {
-        console.log(`${message.member.user.tag} executed /chatchannel ${channel.name} without admin in ${message.guild.name}`);
-        message.reply(':no_entry: You have to be an admin to execute this command.');
+        message.respond(keys.commands.chatchannel.warnings.no_permission);
         return;
     } else if(!channel) {
-        console.log(`${message.member.user.tag} executed /chatchannel without args in ${message.guild.name}`);
-        message.reply(':warning: Please mention a channel.');
+        message.respond(keys.commands.chatchannel.warnings.no_channel);
         return;
     } else if(!channel.isText()) {
-        console.log(`${message.member.user.tag} executed /chatchannel without args in ${message.guild.name}`);
-        message.reply(':warning: Please mention a text, thread or news channel.');
+        message.respond(keys.commands.chatchannel.warnings.no_text_channel);
         return;
     }
 
-    console.log(`${message.member.user.tag} executed /chatchannel ${channel.name} in ${message.guild.name}`);
+    const logChooserMsg = message.respond(keys.commands.chatchannel.success.choose);
 
-
-    const logChooser = new Discord.MessageActionRow()
-        .addComponents(
-            new Discord.MessageSelectMenu()
-                .setCustomId('log')
-                .setMinValues(1)
-                .setMaxValues(8)
-                .setPlaceholder('Select up to 8 message types.')
-                .addOptions([
-                    {
-                        label: 'Chat messages',
-                        description: 'Send a message every time a player chats.',
-                        value: '0',
-                    },
-                    {
-                        label: 'Join messages',
-                        description: 'Send a message every time a player joins the server.',
-                        value: '1',
-                    },
-                    {
-                        label: 'Leave messages',
-                        description: 'Send a message every time a player leaves the server.',
-                        value: '2',
-                    },
-                    {
-                        label: 'Advancement messages',
-                        description: 'Send a message every time a player completes an advancement.',
-                        value: '3',
-                    },
-                    {
-                        label: 'Death messages',
-                        description: 'Send a message every time a player dies.',
-                        value: '4',
-                    },
-                    {
-                        label: 'Command messages',
-                        description: 'Send a message every time a player executes a command.',
-                        value: '5',
-                    },
-                    {
-                        label: 'Startup messages',
-                        description: 'Send a message every time the server starts up.',
-                        value: '6',
-                    },
-                    {
-                        label: 'Shutdown messages',
-                        description: 'Send a message every time the server shuts down.',
-                        value: '7',
-                    },
-                ]),
-    );
-
-    const logChooserMsg = await message.reply({ content: 'Select all message types you want the bot to send. (You have 20 seconds to select)', components: [logChooser] });
     const collector = logChooserMsg.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 20000, max: 1 });
     collector.on('collect', async menu => {
         if(menu.customId === 'log' && menu.member.user.id === message.member.user.id) {
@@ -98,18 +43,23 @@ async function execute(message, args) {
 
             fs.writeFile(`./serverdata/connections/${message.guild.id}/connection.json`, JSON.stringify(pluginJson, null, 2), 'utf-8', err => {
                 if(err) {
-                    console.log('Error writing pluginFile', err);
-                    menu.reply('<:Error:849215023264169985> Couldn\'t save channel. Please try again.');
+                    message.respond(keys.commands.chatchannel.errors.could_not_write_file);
                     return;
                 }
-                console.log('Successfully set chat channel');
-                menu.reply('<:Checkmark:849224496232660992> Successfully set the chat channel');
+
+                console.log(keys.commands.chatchannel.success.final.console);
+
+                const successEmbed = getEmbedBuilder(keys.commands.chatchannel.success.final, ph.fromStd(message));
+                menu.reply({ embeds: [successEmbed] });
             });
-        } else menu.reply({ content: ':warning: Only the command sender can select message types.', ephemeral: true });
+        } else {
+            const notAuthorEmbed = getEmbedBuilder(keys.commands.chatchannel.warnings.not_author, ph.fromStd(message));
+            menu.reply({ embeds: [notAuthorEmbed], ephemeral: true });
+        }
     });
     collector.on('end', collected => {
-        if(!collected.size) message.reply('Select all message types you want the bot to send. (Time ran out)');
-        else message.reply('Select all message types you want the bot to send. (Already responded)');
+        if(!collected.size) message.respond(keys.commands.chatchannel.warnings.not_collected);
+        else message.respond(keys.commands.chatchannel.warnings.already_responded);
     });
 }
 
