823fdd786ab5e91737004e77861d4e223116fe52
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-05-10 14:57:06 +0200

    /message now uses new chatPrivate method

--- commands/other/message.js
+++ commands/other/message.js
@@ -19,34 +19,7 @@ async function execute(message, args) {
     const mcUsername = username.id ? await utils.getUsername(username.id, message) : username;
     if(!mcUsername) return;
 
-    /*[
-        "",
-        {
-            "text":"Discord",
-            "bold":true,
-            "italic":true,
-            "color":"blue",
-            "clickEvent": {
-                "action":"open_url",
-                "value":"https://top.gg/bot/712759741528408064"
-            },
-            "hoverEvent": {
-                "action":"show_text",
-                "contents": [
-                    "Message sent using ",
-                    {
-                        "text":"SMP-Bot",
-                        "color":"gold"1
-                ]
-            }
-        },
-        {
-            "text":" | ${message.member.user.tag} whispers to you: ${chatMsg}",
-            "italic":true
-        }
-    ]*/
-
-    const resp = await plugin.execute(`tellraw ${mcUsername} ["",{"text":"Discord","bold":true,"italic":true,"color":"blue","clickEvent":{"action":"open_url","value":"https://top.gg/bot/712759741528408064"},"hoverEvent":{"action":"show_text","contents":["Message sent using ",{"text":"Minecraft SMP-Bot","color":"gold"}]}},{"text":" | ${message.member.user.tag} whispers to you: ${chatMsg}","italic":true}]`, message);
+    const resp = await plugin.chatPrivate(chatMsg, message.member.user.tag, mcUsername, message);
     if(!resp) return;
 
     if(resp.status === 206) message.respond(keys.commands.message.warnings.response_warning, argPlaceholder, { "response": resp.message });
