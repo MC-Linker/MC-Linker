992a6e790ccaea0e0ef293a6d0b69d90eaacdbac
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-07-01 20:35:02 +0200

    Updated searchStats method

--- api/utils.js
+++ api/utils.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra');
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
+const mcData = require('minecraft-data')('1.19');
 const { keys, addPh, ph } = require('./messages');
 
 function searchAdvancements(searchString, category, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
@@ -68,47 +69,45 @@ function searchAllAdvancements(searchString, shouldSearchNames = true, shouldSea
 
 function searchStats(searchString, category, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
     return new Promise(resolve => {
-        if(category !== 'custom') {
-            fs.readdir(`./resources/images/minecraft/${category}`, (err, images) => {
-                if(err) return resolve(false);
+        let dataList;
+        let matchingStats = [];
 
-                const matchingImages = images.filter(image => image.includes(searchString.replaceAll(' ', '_')));
-                if(matchingImages.length >= 25) matchingImages.length = 25;
+        if(category === 'mined') dataList = mcData.blocksArray;
+        else if(category === 'broken' || category === 'crafted' || category === 'used' || category === 'picked_up' || category === 'dropped') dataList = mcData.itemsArray;
+        else if(category === 'killed' || category === 'killed_by') dataList = mcData.entitiesArray;
 
-                const respondArray = [];
-                matchingImages.forEach(image => {
-                    let formattedImage = image.replaceAll('.png', '');
-                    formattedImage = formattedImage.split('_').map(word => word.cap()).join(' ');
+        if(dataList) {
+            matchingStats = dataList.filter(data => {
+                //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
+                let match = false;
+                if(shouldSearchNames) match = data.displayName.toLowerCase().includes(searchString.toLowerCase());
+                if(!match && shouldSearchValues) match = data.name.includes(searchString.toLowerCase());
 
-                    respondArray.push({
-                        name: formattedImage,
-                        value: image.replaceAll('.png', ''),
-                    });
-                });
-
-                resolve(respondArray);
+                return match;
             });
-        } else {
+
+            matchingStats = [...new Set(matchingStats)]; //Remove duplicates
+            if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
+
+            resolve(matchingStats);
+        } else if(category === 'custom') {
             fs.readJson('./resources/data/stats_custom.json', (err, statData) => {
                 if(err) return resolve([]);
 
-                let matchingStats;
-
-                matchingStats = Object.values(statData.stats).filter(stat => {
+                matchingStats = statData.stats.filter(stat => {
                     //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
-                    let match;
-                    if(shouldSearchNames) match = stat.name.toLowerCase().includes(searchString);
-                    if(shouldSearchValues || !match) match = stat.value.toLowerCase().includes(searchString);
+                    let match = false;
+                    if(shouldSearchNames) match = stat.name.toLowerCase().includes(searchString.toLowerCase());
+                    if(!match && shouldSearchValues) match = stat.value.includes(searchString.toLowerCase());
 
                     return match;
                 });
-
                 matchingStats = [...new Set(matchingStats)]; //Remove duplicates
-                if(matchingStats.length >= maxLength) matchingStats.length = maxLength;
+                if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
 
                 resolve(matchingStats);
             });
-        }
+        } else return resolve([]);
     });
 }
 
