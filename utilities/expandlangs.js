e85741c27de349b1c905418fc6b207f4c468a4e6
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-07-07 16:44:13 +0200

    pl

--- utilities/expandlangs.js
+++ utilities/expandlangs.js
@@ -4,10 +4,10 @@ const _ = require('lodash');
 convertKeys();
 
 function convertKeys() {
-    const languages = fs.readdirSync('../resources/languages/flat');
+    const languages = fs.readdirSync('./resources/languages/flat');
 
     languages.forEach(lang => {
-        const keys = fs.readJsonSync(`../resources/languages/flat/${lang}`, 'utf-8');
+        const keys = fs.readJsonSync(`./resources/languages/flat/${lang}`, 'utf-8');
 
         const converted = {};
         for([key, value] of Object.entries(keys)) {
@@ -15,6 +15,6 @@ function convertKeys() {
         }
 
 
-        fs.writeJsonSync(`../resources/languages/expanded/${lang}`, converted, { spaces: 2 });
+        fs.writeJsonSync(`./resources/languages/expanded/${lang}`, converted, { spaces: 2 });
     });
 }
