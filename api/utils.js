b4b4b95670a206266e65bd5d0de78b79e8cb5a77
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-09-01 13:25:41 +0200

    Made search methods async

--- api/utils.js
+++ api/utils.js
@@ -8,90 +8,84 @@ const { keys, defaultMessage } = require('./messages');
 const advancementData = require('../resources/data/advancements.json');
 const customStats = require('../resources/data/stats_custom.json');
 
-function searchAdvancements(searchString, category, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
-    return new Promise(resolve => {
-        const matchingCategory = advancementData.categories[category];
-        if(!matchingCategory) return resolve([]);
+async function searchAdvancements(searchString, category, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
+    const matchingCategory = advancementData.categories[category];
+    if(!matchingCategory) return [];
 
-        let matchingTitles = matchingCategory.filter(advancement => {
-            //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value
-            let match;
-            if(shouldSearchNames) match = advancement.name.toLowerCase().includes(searchString.toLowerCase());
-            if(shouldSearchValues && !match) match = advancement.value.includes(searchString.toLowerCase());
+    let matchingTitles = matchingCategory.filter(advancement => {
+        //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value
+        let match;
+        if(shouldSearchNames) match = advancement.name.toLowerCase().includes(searchString.toLowerCase());
+        if(shouldSearchValues && !match) match = advancement.value.includes(searchString.toLowerCase());
 
-            return match;
-        });
-
-        //Add category field
-        const categoryKey = Object.keys(advancementData.categories).find(key => advancementData.categories[key] === matchingCategory);
-        matchingTitles.map(title => title.category = categoryKey);
-
-        matchingTitles = [...new Set(matchingTitles)]; //Remove duplicates
-        if(matchingTitles.length >= maxLength) matchingTitles.length = maxLength;
-        resolve(matchingTitles);
+        return match;
     });
+
+    //Add category field
+    const categoryKey = Object.keys(advancementData.categories).find(key => advancementData.categories[key] === matchingCategory);
+    matchingTitles.map(title => title.category = categoryKey);
+
+    matchingTitles = [...new Set(matchingTitles)]; //Remove duplicates
+    if(matchingTitles.length >= maxLength) matchingTitles.length = maxLength;
+    return matchingTitles;
 }
 
-function searchAllAdvancements(searchString, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
-    return new Promise(async resolve => {
-        let matchingTitles = [];
+async function searchAllAdvancements(searchString, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
+    let matchingTitles = [];
 
-        for(const category of Object.keys(advancementData.categories)) {
-            const matchingKeys = await searchAdvancements(searchString, category, shouldSearchNames, shouldSearchValues, maxLength);
-            matchingKeys.forEach(key => matchingTitles.push(key));
-        }
+    for(const category of Object.keys(advancementData.categories)) {
+        const matchingKeys = await searchAdvancements(searchString, category, shouldSearchNames, shouldSearchValues, maxLength);
+        matchingKeys.forEach(key => matchingTitles.push(key));
+    }
 
-        matchingTitles = [...new Set(matchingTitles)]; //Remove duplicates
-        if(matchingTitles.length >= maxLength) matchingTitles.length = maxLength;
-        resolve(matchingTitles);
-    });
+    matchingTitles = [...new Set(matchingTitles)]; //Remove duplicates
+    if(matchingTitles.length >= maxLength) matchingTitles.length = maxLength;
+    return matchingTitles;
 }
 
 function searchStats(searchString, category, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
-    return new Promise(resolve => {
-        let dataList;
-        let matchingStats = [];
+    let dataList;
+    let matchingStats = [];
 
-        if(category === 'mined') dataList = mcData.blocksArray;
-        else if(category === 'broken' || category === 'crafted' || category === 'used' || category === 'picked_up' || category === 'dropped') dataList = mcData.itemsArray;
-        else if(category === 'killed' || category === 'killed_by') dataList = mcData.entitiesArray;
+    if(category === 'mined') dataList = mcData.blocksArray;
+    else if(category === 'broken' || category === 'crafted' || category === 'used' || category === 'picked_up' || category === 'dropped') dataList = mcData.itemsArray;
+    else if(category === 'killed' || category === 'killed_by') dataList = mcData.entitiesArray;
 
-        if(dataList) {
-            matchingStats = dataList.filter(data => {
-                //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
-                let match = false;
-                if(shouldSearchNames) match = data.displayName.toLowerCase().includes(searchString.toLowerCase());
-                if(shouldSearchValues && !match) match = data.name.includes(searchString.toLowerCase());
+    if(dataList) {
+        matchingStats = dataList.filter(data => {
+            //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
+            let match = false;
+            if(shouldSearchNames) match = data.displayName.toLowerCase().includes(searchString.toLowerCase());
+            if(shouldSearchValues && !match) match = data.name.includes(searchString.toLowerCase());
 
-                return match;
-            }).map(data => {
-                return {
-                    name: data.displayName,
-                    value: data.name,
-                };
-            }); //Only include displayName and name
+            return match;
+        }).map(data => {
+            return {
+                name: data.displayName,
+                value: data.name,
+            };
+        }); //Only include displayName and name
 
-            matchingStats = [...new Set(matchingStats)]; //Remove duplicates
-            if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
+        matchingStats = [...new Set(matchingStats)]; //Remove duplicates
+        if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
 
-            resolve(matchingStats);
-        }
-        else if(category === 'custom') {
-            matchingStats = customStats.stats.filter(stat => {
-                //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
-                let match = false;
-                if(shouldSearchNames) match = stat.name.toLowerCase().includes(searchString.toLowerCase());
-                if(!match && shouldSearchValues) match = stat.value.includes(searchString.toLowerCase());
+        return matchingStats;
+    }
+    else if(category === 'custom') {
+        matchingStats = customStats.stats.filter(stat => {
+            //Filter (if shouldSearchNames === true) for matching name and (if shouldSearchValues === true) for matching value or category.value
+            let match = false;
+            if(shouldSearchNames) match = stat.name.toLowerCase().includes(searchString.toLowerCase());
+            if(!match && shouldSearchValues) match = stat.value.includes(searchString.toLowerCase());
 
-                return match;
-            });
-            matchingStats = [...new Set(matchingStats)]; //Remove duplicates
-            if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
+            return match;
+        });
+        matchingStats = [...new Set(matchingStats)]; //Remove duplicates
+        if(matchingStats.length >= maxLength) matchingStats.length = maxLength; //Reduce length
 
-            resolve(matchingStats);
-        }
-        else return resolve([]);
-    });
+        return matchingStats;
+    }
+    else return [];
 }
 
 function searchAllStats(searchString, shouldSearchNames = true, shouldSearchValues = true, maxLength = 25) {
