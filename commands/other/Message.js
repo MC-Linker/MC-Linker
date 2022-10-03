a5594f60104e0c99a2bdeb05b8845a1040984e08
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-10-03 11:27:56 +0200

    `/message` now works with new structures

--- commands/other/Message.js
+++ commands/other/Message.js
@@ -4,33 +4,28 @@ const Command = require('../../structures/Command');
 class Message extends Command {
 
     constructor() {
-        super('message', true, true);
+        super({
+            name: 'message',
+            ephemeral: true,
+            requiresConnectedPlugin: true,
+            requiresConnectedUser: 0,
+        });
     }
 
 
-    async execute(interaction, client, args) {
-        const username = interaction.mentions.users.first() ?? args[0];
+    async execute(interaction, client, args, server) {
+        if(!await super.execute(interaction, client, args, server)) return;
+
+        const user = args[0];
         args.shift(); //Remove username from args
         const chatMsg = args?.join(' ').replaceAll(`"`, `\\"`);
-        const argPlaceholder = { username, 'message': chatMsg };
+        const argPlaceholder = { username: user.username, 'message': chatMsg };
 
-        if(!username) {
-            interaction.replyTl(keys.commands.message.warnings.no_username);
-            return;
-        }
-        else if(!chatMsg) {
-            interaction.replyTl(keys.commands.message.warnings.no_message);
-            return;
-        }
-
-        const mcUsername = username.id ? await utils.getUsername(username.id, interaction) : username;
-        if(!mcUsername) return;
-
-        const resp = await plugin.chatPrivate(chatMsg, interaction.guildId, interaction.member.user.tag, mcUsername, interaction);
+        const resp = await server.protocol.chatPrivate(chatMsg, user.username);
         if(!resp) return;
 
-        if(resp.status === 206) interaction.replyTl(keys.commands.message.warnings.response_warning, argPlaceholder, { 'response': resp.json.message });
-        else interaction.replyTl(keys.commands.message.success, argPlaceholder);
+        if(resp.status === 206) return interaction.replyTl(keys.commands.message.warnings.response_warning, argPlaceholder, { 'response': resp.json.message });
+        else return interaction.replyTl(keys.commands.message.success, argPlaceholder);
     }
 }
 
