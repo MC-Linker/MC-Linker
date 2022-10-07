c1c2e8e9e554c0304c0341dda8710023094e28f7
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-10-07 12:54:53 +0200

    Added Button structure

---
+++ structures/Button.js
@@ -0,0 +1,59 @@
+const { ButtonInteraction } = require('discord.js');
+const { keys } = require('../api/messages');
+const { PermissionsBitField, User } = require('discord.js');
+
+class Button {
+
+    /**
+     * @typedef {Object} ButtonOptions
+     * @property {string} id - The first part of the button's custom ID.
+     * @property {PermissionsBitField} [permissions] - The permissions required to use this button.
+     * @property {User} [author] - The author of this button that is allowed to use it.
+     * @property {boolean} [ephemeral=false] - Whether this button should be ephemeral.
+     * @property {boolean} [defer=true] - Whether this button should be deferred.
+     */
+
+    /**
+     * Creates a new Button instance.
+     * @param {ButtonOptions} options - The options for this command.
+     */
+    constructor(options) {
+        this.id = options.id;
+        this.permissions = options.permissions;
+        this.author = options.author;
+        this.ephemeral = options.ephemeral ?? false;
+        this.defer = options.defer ?? true;
+    }
+
+    /**
+     * Handles the execution of this button.
+     * @param {ButtonInteraction & TranslatedResponses} interaction - The button interaction.
+     * @param {MCLinker} client - The MCLinker client.
+     * @param {?ServerConnection} server - The connection of the server the command was executed in.
+     * @returns {Promise<?boolean>|?boolean}
+     * @abstract
+     */
+    async execute(interaction, client, server) {
+        if(this.defer) await interaction.deferReply({ ephemeral: this.ephemeral });
+
+        if(this.permissions) {
+            const memberPerms = interaction.member.permissionsIn(interaction.channel);
+            if(!memberPerms.has(this.permissions)) {
+                const missingPermission = this.permissions.toArray().find(perm => !memberPerms.has(perm));
+                await interaction.replyTl(keys.api.button.warnings.no_permission, { permission: missingPermission });
+                return false;
+            }
+        }
+
+        if(this.author) {
+            if(this.author.id !== interaction.user.id) {
+                await interaction.replyTl(keys.api.button.warnings.no_author);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = Button;
