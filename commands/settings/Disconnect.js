6446940ee1d06a56ec9d2601f63087d4685c341d
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-10-30 13:52:01 +0100

    Removed subcommands of /disconnect

--- commands/settings/Disconnect.js
+++ commands/settings/Disconnect.js
@@ -16,23 +16,14 @@ class Disconnect extends Command {
     async execute(interaction, client, args, server) {
         if(!await super.execute(interaction, client, args, server)) return;
 
-        const method = args[0];
-
         const protocol = server.protocol instanceof PluginProtocol ? 'plugin' : 'ftp';
-        if(protocol === 'plugin' && method === 'ftp') {
-            return interaction.replyTl(keys.api.command.errors.server_not_connected_ftp);
-        }
-        else if(protocol === 'ftp' && method === 'plugin') {
-            return interaction.replyTl(keys.api.command.errors.server_not_connected_plugin);
-        }
-
-        if(method === 'plugin') {
+        if(protocol === 'plugin') {
             const disconnect = await server.protocol.disconnect();
             if(!await utils.handleProtocolResponse(disconnect, server.protocol, interaction)) return;
         }
 
         await client.serverConnections.disconnect(server);
-        return interaction.replyTl(keys.commands.disconnect.success, { method, method_cap: method.cap() });
+        return interaction.replyTl(keys.commands.disconnect.success, { protocol, protocol_cap: protocol.cap() });
     }
 }
 
