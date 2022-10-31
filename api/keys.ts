20152d512f9d2d882f489c2b963105ea1c45e8a7
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-10-31 12:31:04 +0100

    Updated tsconfig

--- api/keys.ts
+++ api/keys.ts
@@ -10,27 +10,6 @@ import {
 const keysFile = require('../resources/languages/expanded/en_us.json');
 export const keys = keysFile;
 
-/*function initKeys(): typeof keysFile {
- const keys = addKeys(keysFile);
-
- function addKeys(obj, parentKey = null) {
- const keys = {};
- for(const key of Object.keys(obj)) {
- keys[key] = parentKey ? `${parentKey}.${key}` : key;
- }
- return keys;
- }
-
- const handler: ProxyHandler<any> = {
- get(target, name: string) {
- const value = _.get(keysFile, target[name]);
- return typeof value === 'string' ? value : new Proxy(addKeys(value, target[name]), handler);
- },
- };
-
- return new Proxy(keys, handler);
- }*/
-
 export interface TranslatedResponses {
     replyTl(key: string, ...placeholders: Object[]): Promise<Message | InteractionResponse>,
 
