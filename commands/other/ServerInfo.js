33ff3e94d2d616adf46f961c88afa417f2b7ce66
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-11-05 21:36:41 +0100

    Added pagination to serverinfo command

--- commands/other/ServerInfo.js
+++ commands/other/ServerInfo.js
@@ -4,7 +4,10 @@ const Protocol = require('../../structures/Protocol');
 const path = require('path');
 const Discord = require('discord.js');
 const utils = require('../../api/utils');
-const { getEmbed, addPh } = require('../../api/messages');
+const { addPh, getComponent, getReplyOptions } = require('../../api/messages');
+const Canvas = require('@napi-rs/canvas');
+const fs = require('fs-extra');
+const Pagination = require('../../structures/helpers/Pagination');
 
 
 class ServerInfo extends Command {
@@ -38,24 +41,98 @@ class ServerInfo extends Command {
         if(onlinePlayers === null || onlinePlayers.status !== 200) onlinePlayers = 0;
         else onlinePlayers = onlinePlayers.data.length;
 
+        const serverIp = propertiesObject['server-ip'] ?? server.protocol.ip;
+        const serverName = propertiesObject['server-name'] ?? serverIp;
 
-        const serverIp = propertiesObject['server-ip'] !== '' ? propertiesObject['server-ip'] : server.protocol.ip;
-        const iconAttachment = serverIcon.status === 200 ? [new Discord.AttachmentBuilder(serverIcon.data, {
+        const motd = propertiesObject['motd']
+            .replace(/\\u00A7/gi, '\u00A7')
+            .replace('\\n', '\n')
+            .split('\n');
+
+        const listCanvas = Canvas.createCanvas(869, 128);
+        const ctx = listCanvas.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
+
+        const background = await Canvas.loadImage('./resources/images/backgrounds/menu_dark.png');
+        ctx.drawImage(background, 0, 0, listCanvas.width, listCanvas.height);
+        const onlineIcon = await Canvas.loadImage('./resources/images/misc/online.png');
+        ctx.drawImage(onlineIcon, listCanvas.width - 40, 4, 32, 32);
+        const iconBuffer = serverIcon.data ? serverIcon.data : await fs.readFile('./resources/images/misc/unknown_server.png');
+        const iconImage = await Canvas.loadImage(iconBuffer);
+        ctx.drawImage(iconImage, 0, 0, 128, 128);
+
+        ctx.font = '28px Minecraft';
+        ctx.fillStyle = '#fff';
+
+        //Draw server name
+        this.drawShadowText(ctx, serverName, 128, 32);
+
+        //Draw motd
+        ctx.textAlign = 'center';
+        if(motd.length === 1) {
+            //Put first line on second line if only one line
+            motd[1] = motd[0];
+            motd[0] = '';
+        }
+        ctx.save();
+        utils.drawMinecraftText(ctx, motd[0], listCanvas.width / 2 - 32, 70);
+        utils.drawMinecraftText(ctx, motd[1], listCanvas.width / 2 - 32, 102);
+        ctx.restore();
+
+        //Draw online players
+        ctx.textAlign = 'right';
+        const playerText = `${onlinePlayers}/${propertiesObject['max-players']}`;
+        const textMeasure = ctx.measureText(playerText);
+        this.drawShadowText(ctx, playerText, listCanvas.width - 8 - textMeasure.width, 32);
+
+        const iconAttachment = new Discord.AttachmentBuilder(iconBuffer, {
             name: 'server-icon.png',
             description: 'Server Icon',
-        })] : [];
-        return interaction.replyOptions({
-            embeds: [getEmbed(keys.commands.serverinfo.success, {
-                server_name: propertiesObject['server-name'] ?? keys.commands.serverinfo.warnings.unknown_server_name,
-                motd: propertiesObject['motd'],
-                max_players: propertiesObject['max-players'],
-                online_players: onlinePlayers,
-                ip: serverIp,
-                version: datObject.Data.Version.Name,
-            })],
-            files: iconAttachment,
+        });
+        const serverListAttachment = new Discord.AttachmentBuilder(listCanvas.toBuffer('image/png'), {
+            name: 'server-list.png',
+            description: 'Server List',
         });
 
+        /** @type {Discord.InteractionReplyOptions} */
+        const startingMessage = {
+            embeds: [keys.commands.serverinfo.success.general],
+            files: [iconAttachment, serverListAttachment],
+        };
+
+        /** @type {PaginationPages} */
+        const pages = {
+            serverinfo_general: {
+                button: getComponent(keys.commands.serverinfo.success.general_button),
+                page: getReplyOptions(startingMessage, {
+                    server_name: propertiesObject['server-name'] ?? keys.commands.serverinfo.warnings.unknown_server_name,
+                    motd: motd.join('\n'),
+                    max_players: propertiesObject['max-players'],
+                    online_players: onlinePlayers,
+                    ip: serverIp,
+                    version: datObject.Data.Version.Name,
+                }),
+                startPage: true,
+            },
+            serverinfo_world: {
+                button: getComponent(keys.commands.serverinfo.success.world_button),
+                page: getReplyOptions(keys.commands.serverinfo.success.world, {}),
+            },
+            serverinfo_admin: {
+                button: getComponent(keys.commands.serverinfo.success.admin_button),
+                page: getReplyOptions(keys.commands.serverinfo.success.admin, {}),
+            },
+        };
+
+        const pagination = new Pagination(client, interaction, pages);
+        return pagination.start();
+    }
+
+    drawShadowText(ctx, text, x, y) {
+        ctx.fillStyle = '#000';
+        ctx.fillText(text, x + 4, y + 4);
+        ctx.fillStyle = '#fff';
+        ctx.fillText(text, x, y);
     }
 }
 
