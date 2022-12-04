902931c09d32304332fd1fb81b9c031970677549
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-12-04 21:46:24 +0100

    Updated tsconfig

--- api/keys.ts
+++ api/keys.ts
@@ -6,14 +6,12 @@ import {
     MessageReplyOptions,
     WebhookEditMessageOptions,
 } from 'discord.js';
-import keysFile from '../resources/languages/expanded/en_us.json' assert { type: 'json' };
+import keys from '../resources/languages/expanded/en_us.json' assert { type: 'json' };
 
-const keys = keysFile;
+export default keys;
 
 export interface TranslatedResponses {
     replyTl(key: string, ...placeholders: Object[]): Promise<Message | InteractionResponse>,
 
     replyOptions(options: string | MessagePayload | InteractionReplyOptions | WebhookEditMessageOptions | MessageReplyOptions): Promise<Message | InteractionResponse>,
 }
-
-export default keys;
