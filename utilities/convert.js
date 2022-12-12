37fcf015a30b0d04f0bb4a3858a2c5f152ecb7eb
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-12-12 12:43:15 +0100

    Fixes for convert

--- utilities/convert.js
+++ utilities/convert.js
@@ -7,10 +7,16 @@ for(const folder of serverFolders) {
     console.group(`Converting server ${folder}...`);
     if(!fs.existsSync(`./serverdata/connections/${folder}/connection.json`)) {
         console.log('No connection.json found, skipping.');
+        console.groupEnd();
         continue;
     }
     const connection = fs.readJsonSync(`./serverdata/connections/${folder}/connection.json`);
     const newConnection = { ...connection };
+    if(connection.worldPath) {
+        console.log('World path found, skipping.');
+        console.groupEnd();
+        continue;
+    }
 
     console.log('Old Path:', connection.path);
     connection.path = decodeURIComponent(connection.path);
@@ -18,7 +24,7 @@ for(const folder of serverFolders) {
     newConnection.path = path.dirname(connection.path);
     console.log('New Paths:', { path: newConnection.path, worldPath: newConnection.worldPath });
 
-    fs.writeJsonSync(`./serverdata-new/connections/${folder}/connection.json`, newConnection, { spaces: 2 });
+    fs.writeJsonSync(`./serverdata/connections/${folder}/connection.json`, newConnection, { spaces: 2 });
     console.log(`Converted server ${folder}!`);
     console.groupEnd();
 }
