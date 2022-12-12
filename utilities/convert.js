fcf77d0812586330e814c9a02ed1ca7e6e974edf
Lianecx <84291452+Lianecx@users.noreply.github.com>
2022-12-12 12:31:17 +0100

    Added logs to convert

--- utilities/convert.js
+++ utilities/convert.js
@@ -4,13 +4,17 @@ import path from 'path';
 
 const serverFolders = fs.readdirSync('./serverdata/connections');
 for(const folder of serverFolders) {
+    console.group(`Converting server ${folder}...`);
     const connection = fs.readJsonSync(`./serverdata/connections/${folder}/connection.json`);
-
     const newConnection = { ...connection };
 
+    console.log('Old Path:', connection.path);
     connection.path = decodeURIComponent(connection.path);
     newConnection.worldPath = connection.path;
     newConnection.path = path.dirname(connection.path);
+    console.log('New Paths:', { path: newConnection.path, worldPath: newConnection.worldPath });
 
-    fs.writeJsonSync(`./serverdata/connections/${folder}/connection.json`, newConnection, { spaces: 2 });
+    fs.writeJsonSync(`./serverdata-new/connections/${folder}/connection.json`, newConnection, { spaces: 2 });
+    console.log(`Converted server ${folder}!`);
+    console.groupEnd();
 }
