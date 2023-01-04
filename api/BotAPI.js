01100426746310d804a66fd6a39ed9d0dfd2912e
Lianecx <84291452+Lianecx@users.noreply.github.com>
2023-01-04 18:30:31 +0100

    Implemented WebSocketProtocol and Websocket server
    Signed-off-by: Lianecx <84291452+Lianecx@users.noreply.github.com>

--- api/BotAPI.js
+++ api/BotAPI.js
@@ -7,8 +7,15 @@ import keys from './keys.js';
 import PluginProtocol from '../structures/PluginProtocol.js';
 import { EventEmitter } from 'node:events';
 import fastifyCookie from '@fastify/cookie';
+import fastifyIO from 'fastify-socket.io';
 
 export default class BotAPI extends EventEmitter {
+
+    /**
+     * The websocket instance for the api.
+     * @type {import('socket.io').Server}
+     */
+    websocket;
 
     constructor(client) {
         super();
@@ -25,7 +32,7 @@ export default class BotAPI extends EventEmitter {
          * @type {import('fastify').FastifyInstance}
          */
         this.fastify = Fastify();
-
+        this.fastify.register(fastifyIO);
         this.fastify.register(fastifyCookie, { secret: process.env.COOKIE_SECRET });
 
         this.fastify.addHook('preHandler', (request, reply, done) => {
@@ -35,138 +42,17 @@ export default class BotAPI extends EventEmitter {
     }
 
     async startServer() {
-        this.fastify.post('/chat', async (request, reply) => {
-            const message = request.body.message;
-            const channels = request.body.channels;
-            const guildId = request.body.id;
-            const type = request.body.type;
-            const player = request.body.player?.replaceAll(' ', '');
-            const authorURL = `https://minotar.net/helm/${player}/64.png`;
-            const argPlaceholder = { ip: request.body.ip, 'username': player, 'author_url': authorURL, message };
+        this.fastify.post('/chat', (request, reply) => {
+            const { id: guildId, ip, port } = request.body;
 
-            const ip = request.body.ip.split(':')[0];
-            const port = request.body.ip.split(':')[1];
             /** @type {ServerConnection} */
             const server = this.client.serverConnections.cache.find(server => server.id === guildId && server.ip === ip && server.port === port && server.protocol instanceof PluginProtocol);
 
-            //If no connection on that guild
+            //If no connection on that guild send disconnection status
             if(!server) return reply.status(403).send();
             else reply.send({});
 
-            //Check whether command is blocked
-            const commandName = message.replace(/^\//, '').split(/\s+/)[0];
-            if(
-                ['player_command', 'console_command', 'block_command'].includes(type) &&
-                server.settings.disabled['chat-commands']?.some(cmd => cmd === commandName)
-            ) return;
-
-            let chatEmbed;
-            if(type === 'advancement') {
-                let advancementTitle;
-                let advancementDesc;
-
-                if(message.startsWith('minecraft:recipes')) return; //Dont process recipes
-
-                const [category, id] = message.replace('minecraft:', '').split('/');
-                const advancement = utils.searchAdvancements(id, category, false, true, 1);
-
-                advancementTitle = advancement[0]?.name ?? message;
-                advancementDesc = advancement[0]?.description ?? keys.commands.advancements.no_description_available;
-
-                chatEmbed = getEmbed(keys.api.plugin.success.messages.advancement, argPlaceholder, {
-                    'advancement_title': advancementTitle,
-                    'advancement_description': advancementDesc,
-                });
-            }
-            else if(type === 'chat') {
-                const guild = await this.client.guilds.fetch(guildId);
-
-                //Parse pings (@name)
-                let mentions = message.match(/@(\S+)/g);
-                for(const mention of mentions ?? []) {
-                    const users = await guild.members.search({ query: mention.replace('@', '') });
-                    argPlaceholder.message = argPlaceholder.message.replace(mention, users.first()?.toString() ?? mention);
-                }
-
-                chatEmbed = getEmbed(keys.api.plugin.success.messages.chat, argPlaceholder, ph.emojis());
-
-                let allWebhooks;
-                try {
-                    allWebhooks = await guild.fetchWebhooks();
-                }
-                catch(_) {}
-
-                for(const channel of channels) {
-                    const discordChannel = await this.client.channels.fetch(channel.id);
-                    if(!discordChannel) continue;
-
-                    if(!allWebhooks) {
-                        discordChannel?.send({ embeds: [getEmbed(keys.api.plugin.errors.no_webhook_permission, ph.emojis())] });
-                        return;
-                    }
-
-                    if(!channel.webhook) {
-                        try {
-                            discordChannel.send({ embeds: [chatEmbed] })
-                                .catch(() => {});
-                        }
-                        catch(_) {}
-                        continue;
-                    }
-
-                    let webhook = allWebhooks.get(channel.webhook);
-
-                    //Create new webhook if old one doesn't exist
-                    if(!webhook) {
-                        const options = {
-                            name: player,
-                            reason: 'ChatChannel to Minecraft',
-                            avatar: authorURL,
-                        };
-
-                        if(discordChannel.isThread()) webhook = await discordChannel.parent.createWebhook(options);
-                        else webhook = await discordChannel.createWebhook(options);
-
-                        const regChannel = await server.protocol.addChatChannel({
-                            types: channel.types,
-                            webhook: webhook.id,
-                            id: channel.id,
-                        });
-                        if(!regChannel) {
-                            discordChannel.send({ embeds: [getEmbed(keys.api.plugin.errors.could_not_add_webhook, ph.emojis())] });
-                            await webhook.delete();
-                            return;
-                        }
-
-
-                        await server.edit({ channels: regChannel.data.channels });
-                    }
-
-                    //Edit webhook if name doesnt match
-                    if(webhook.name !== player) {
-                        await webhook.edit({ name: player, avatar: authorURL });
-                    }
-
-                    if(discordChannel.isThread()) await webhook.send({
-                        threadId: discordChannel.id,
-                        content: argPlaceholder.message,
-                    });
-                    else await webhook.send(argPlaceholder.message);
-                }
-                return;
-            }
-            else {
-                chatEmbed = getEmbed(keys.api.plugin.success.messages[type], argPlaceholder, ph.emojis(), { 'timestamp_now': Date.now() });
-            }
-
-            try {
-                for(const channel of channels) {
-                    const discordChannel = await this.client.channels.fetch(channel.id);
-                    await discordChannel?.send({ embeds: [chatEmbed] })
-                        .catch(() => {});
-                }
-            }
-            catch(_) {}
+            this._chat(request.body, server);
         });
 
         this.fastify.get('/linked-role', async (request, reply) => {
@@ -228,6 +114,147 @@ export default class BotAPI extends EventEmitter {
             console.log(addPh(keys.api.plugin.success.listening.console, { address }));
         });
 
+        await this.fastify.ready(); //Await websocket plugin loading
+        this.websocket = this.fastify.io;
+
+        this.websocket.on('connection', socket => {
+            console.log('Socket connected', socket);
+
+            socket.on('chat', data => {
+                const { id: guildId, ip, port } = data;
+
+                /** @type {ServerConnection} */
+                const server = this.client.serverConnections.cache.find(server => server.id === guildId && server.ip === ip && server.port === port && server.protocol instanceof PluginProtocol);
+
+                //If no connection on that guild, disconnect socket
+                if(!server) socket.disconnect();
+
+                this._chat(data, server);
+            });
+        });
         return this.fastify;
+    }
+
+    async _chat(data, server) {
+        const { message, channels, id: guildId, type, player } = data;
+        const authorURL = `https://minotar.net/helm/${player}/64.png`;
+        const ip = request.body.ip.split(':')[0];
+
+        const argPlaceholder = { ip, username: player, author_url: authorURL, message };
+
+        //Check whether command is blocked
+        const commandName = message.replace(/^\//, '').split(/\s+/)[0];
+        if(
+            ['player_command', 'console_command', 'block_command'].includes(type) &&
+            server.settings.disabled['chat-commands']?.some(cmd => cmd === commandName)
+        ) return;
+
+        let chatEmbed;
+        if(type === 'advancement') {
+            let advancementTitle;
+            let advancementDesc;
+
+            if(message.startsWith('minecraft:recipes')) return; //Dont process recipes
+
+            const [category, id] = message.replace('minecraft:', '').split('/');
+            const advancement = utils.searchAdvancements(id, category, false, true, 1);
+
+            advancementTitle = advancement[0]?.name ?? message;
+            advancementDesc = advancement[0]?.description ?? keys.commands.advancements.no_description_available;
+
+            chatEmbed = getEmbed(keys.api.plugin.success.messages.advancement, argPlaceholder, {
+                'advancement_title': advancementTitle,
+                'advancement_description': advancementDesc,
+            });
+        }
+        else if(type === 'chat') {
+            const guild = await this.client.guilds.fetch(guildId);
+
+            //Parse pings (@name)
+            let mentions = message.match(/@(\S+)/g);
+            for(const mention of mentions ?? []) {
+                const users = await guild.members.search({ query: mention.replace('@', '') });
+                argPlaceholder.message = argPlaceholder.message.replace(mention, users.first()?.toString() ?? mention);
+            }
+
+            chatEmbed = getEmbed(keys.api.plugin.success.messages.chat, argPlaceholder, ph.emojis());
+
+            let allWebhooks;
+            try {
+                allWebhooks = await guild.fetchWebhooks();
+            }
+            catch(_) {}
+
+            for(const channel of channels) {
+                const discordChannel = await this.client.channels.fetch(channel.id);
+                if(!discordChannel) continue;
+
+                if(!allWebhooks) {
+                    discordChannel?.send({ embeds: [getEmbed(keys.api.plugin.errors.no_webhook_permission, ph.emojis())] });
+                    return;
+                }
+
+                if(!channel.webhook) {
+                    try {
+                        discordChannel.send({ embeds: [chatEmbed] })
+                            .catch(() => {});
+                    }
+                    catch(_) {}
+                    continue;
+                }
+
+                let webhook = allWebhooks.get(channel.webhook);
+
+                //Create new webhook if old one doesn't exist
+                if(!webhook) {
+                    const options = {
+                        name: player,
+                        reason: 'ChatChannel to Minecraft',
+                        avatar: authorURL,
+                    };
+
+                    if(discordChannel.isThread()) webhook = await discordChannel.parent.createWebhook(options);
+                    else webhook = await discordChannel.createWebhook(options);
+
+                    const regChannel = await server.protocol.addChatChannel({
+                        types: channel.types,
+                        webhook: webhook.id,
+                        id: channel.id,
+                    });
+                    if(!regChannel) {
+                        discordChannel.send({ embeds: [getEmbed(keys.api.plugin.errors.could_not_add_webhook, ph.emojis())] });
+                        await webhook.delete();
+                        return;
+                    }
+
+
+                    await server.edit({ channels: regChannel.data.channels });
+                }
+
+                //Edit webhook if name doesnt match
+                if(webhook.name !== player) {
+                    await webhook.edit({ name: player, avatar: authorURL });
+                }
+
+                if(discordChannel.isThread()) await webhook.send({
+                    threadId: discordChannel.id,
+                    content: argPlaceholder.message,
+                });
+                else await webhook.send(argPlaceholder.message);
+            }
+            return;
+        }
+        else {
+            chatEmbed = getEmbed(keys.api.plugin.success.messages[type], argPlaceholder, ph.emojis(), { 'timestamp_now': Date.now() });
+        }
+
+        try {
+            for(const channel of channels) {
+                const discordChannel = await this.client.channels.fetch(channel.id);
+                await discordChannel?.send({ embeds: [chatEmbed] })
+                    .catch(() => {});
+            }
+        }
+        catch(_) {}
     }
 }
---
+++ structures/WebSocketProtocol.js
@@ -0,0 +1,206 @@
+import Protocol from './Protocol.js';
+import fs from 'fs-extra';
+
+export default class WebSocketProtocol extends Protocol {
+
+    /**
+     * @typedef {object} WebSocketProtocolData
+     * @property {string} ip - The ip the websocket client is listening on.
+     * @property {number} port - The port the websocket client is listening on.
+     * @property {string} id - The guild id this protocol is for.
+     */
+
+    /**
+     * The connected WebSocket of this protocol.
+     * @type {?import('socket.io').Socket}
+     */
+    socket;
+
+    /**
+     * Creates a new protocol.
+     * @param {MCLinker} client - The client to create the protocol for.
+     * @param {WebSocketProtocolData} data - The data for the protocol.
+     */
+    constructor(client, data) {
+        super(client);
+
+        this._patch(data);
+    }
+
+    _patch(data) {
+
+        /**
+         * The guild id this protocol is for.
+         */
+        this.id = data.id ?? this.id;
+
+        /**
+         * The ip the websocket client is listening on.
+         * @type {string}
+         */
+        this.ip = data.ip ?? this.ip;
+
+        /**
+         * The port the websocket client is listening on.
+         * @type {number}
+         */
+        this.port = data.port ?? this.port;
+    }
+
+    /**
+     * Sends a verification request to the server. Users can verify using `/verify <code>`.
+     * @param {string} code - The verification code to send.
+     * @param {string} uuid - The uuid of the user that sent the request.
+     * @returns {Promise<?ProtocolResponse>} - The response from the websocket client.
+     */
+    async verifyUser(code, uuid) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('verify', { code, uuid }, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Disconnects from the plugin.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async disconnect() {
+        await this.socket.disconnect();
+        return { status: 200 };
+    }
+
+    /**
+     * @inheritDoc
+     */
+    async get(getPath, putPath) {
+        return new Promise(async resolve => {
+            await fs.ensureFile(putPath);
+
+            this.socket.timeout(5000).emit('get-file', { path: getPath }, async (err, response) => {
+                if(response?.status !== 200) return resolve(null);
+
+                await fs.writeFile(putPath, response.data);
+                resolve({
+                    status: response.status,
+                    data: await fs.readFile(putPath),
+                });
+            });
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    async put(getPath, putPath) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('put-file', await fs.readFile(getPath), (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    async list(folder) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('list-file', { folder }, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Sends a public chat message to the server.
+     * @param {string} message - The content of the message to send.
+     * @param {string} username - The user who sent the message.
+     * @param {?string=null} replyMessage - THe content of the message that was replied to.
+     * @param {?string=null} replyUsername - The username of the user who sent the message that was replied to.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async chat(message, username, replyMessage = null, replyUsername = null) {
+        return new Promise(async resolve => {
+            const data = {
+                msg: message,
+                username,
+                private: false,
+            };
+            if(replyMessage && replyUsername) {
+                data.reply_msg = replyMessage;
+                data.reply_username = replyUsername;
+            }
+
+            this.socket.timeout(5000).emit('chat', data, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Sends a private chat message to a player on the server.
+     * @param {string} message - The message to send.
+     * @param {string} username - The user who sent the message.
+     * @param {string} target - The username of the player to send the message to.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async chatPrivate(message, username, target) {
+        return new Promise(async resolve => {
+            const data = { msg: message, username, target, private: true };
+            this.socket.timeout(5000).emit('chat', data, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Adds a chat channel to the server.
+     * @param {ChatChannelData} channel - The chat channel to add.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async addChatChannel(channel) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('add-channel', channel, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Removes a chat channel from the server.
+     * @param {ChatChannelData} channel - The chat channel to remove.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async removeChatChannel(channel) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('remove-channel', channel, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Executes a command on the server.
+     * @param {string} command - The command to execute.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async execute(command) {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('command', { command }, (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+
+    /**
+     * Gets a list of online players on the server.
+     * @returns {Promise<?ProtocolResponse>} - The response from the plugin.
+     */
+    async getOnlinePlayers() {
+        return new Promise(async resolve => {
+            this.socket.timeout(5000).emit('list-players', (err, response) => {
+                resolve(response);
+            });
+        });
+    }
+}
