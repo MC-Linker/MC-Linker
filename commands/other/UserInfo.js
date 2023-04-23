cc8fb8ba5890a6cecdeb43015082fb8c12c24ac6
Lianecx <84291452+Lianecx@users.noreply.github.com>
2023-04-23 15:52:23 +0200

    more fixsxx
    Signed-off-by: Lianecx <84291452+Lianecx@users.noreply.github.com>

--- commands/other/UserInfo.js
+++ commands/other/UserInfo.js
@@ -107,15 +107,15 @@ export default class UserInfo extends Command {
             else if(playTimeMinutes) playTimeSeconds = playTimeMinutes * 60;
             placeholders.play_time = time(playTimeSeconds);
 
-            newGeneralFields.push(...(addPh(
+            newGeneralFields.push(addPh(
                 keys.commands.userinfo.success.general.embeds[0].fields[9], placeholders,
-            )));
+            ));
         }
         if(scoreboardDat) {
             placeholders.teams = scoreboardDatObject.Teams.filter(team => team.Players.includes(user.username)).map(team => team.DisplayName).join(', ');
-            newGeneralFields.push(...(addPh(
+            newGeneralFields.push(addPh(
                 keys.commands.userinfo.success.general.embeds[0].fields[10], placeholders,
-            )));
+            ));
         }
         generalEmbed.setFields(...newGeneralFields);
         adminEmbed.setFields(...newAdminFields);
