719121ae3d086fea649b1404248c99deafebd609
Lianecx <84291452+Lianecx@users.noreply.github.com>
2023-11-13 23:17:11 +0100

    Removed unused methods
    Signed-off-by: Lianecx <84291452+Lianecx@users.noreply.github.com>

--- structures/Command.js
+++ structures/Command.js
@@ -1,5 +1,4 @@
 import {
-    ApplicationCommand,
     ApplicationCommandOptionType,
     ApplicationCommandPermissionType,
     CommandInteraction,
@@ -110,21 +109,6 @@ export default class Command {
             return false;
         }
 
-        /*        const slashCommand = await fetchCommand(client.application.commands, this.name);
-                const missingPermission = await canRunCommand(slashCommand);
-                if(missingPermission !== true) {
-                    if(typeof missingPermission === 'string') {
-                        await interaction.replyTl(
-                            keys.api.command.no_access.no_permission,
-                            { permission: missingPermission },
-                        );
-                    }
-                    else {
-                        await interaction.replyTl(keys.api.command.no_access.no_unknown_permission);
-                    }
-                    return false;
-                }*/
-
         if(this.requiresUserIndex !== null && (this.requiresUserIndex === 0 || args[this.requiresUserIndex - 1] !== undefined)) {
             const user = await client.userConnections.userFromArgument(args[this.requiresUserIndex], server);
             if(user.error === 'nullish') {
@@ -143,93 +127,6 @@ export default class Command {
             args[this.requiresUserIndex] = user;
         }
 
-/*        const optionName = await getMissingOptionName(args, slashCommand);
-        if(optionName) {
-            await interaction.replyTl(
-                keys.api.command.warnings.no_argument,
-                { argument: optionName },
-            );
-
-            return false;
-        }
-
-        //Parse booleans and channels in arguments
-        for(let item of args) {
-            if(item === 'true') item = true;
-            else if(item === 'false') item = false;
-            else {
-                const channelId = MessageMentions.ChannelsPattern.exec(item);
-                item = channelId ? await client.channels.fetch(channelId[1]) : item;
-            }
-        }*/
-
         return true;
-
-        async function getMissingOptionName(args, command) {
-            const subcommandGroup = args[0];
-            const subcommand = args[1];
-
-            let option;
-            const subcommandGroupOption = command.options.find(option => option.name === subcommandGroup);
-            if(subcommandGroupOption?.type === ApplicationCommandOptionType.SubcommandGroup || subcommandGroupOption?.type === ApplicationCommandOptionType.Subcommand) {
-                const subcommandOption = subcommandGroupOption.options?.find(option => option.name === subcommand);
-                if(subcommandOption?.type === ApplicationCommandOptionType.Subcommand) {
-                    option = subcommandOption?.options?.[args.length - 2];
-                }
-                else option = subcommandGroupOption.options?.[args.length - 1];
-            }
-            else {
-                const missingOption = command.options[args.length];
-                if(missingOption?.type === ApplicationCommandOptionType.SubcommandGroup || missingOption?.type === ApplicationCommandOptionType.Subcommand) {
-                    option = command.options;
-                }
-                else option = missingOption;
-            }
-
-            if(Array.isArray(option)) {
-                return new Intl.ListFormat('en', { type: 'disjunction', style: 'short' })
-                    .format(option.map(option => option.name));
-            }
-            else if(option?.required) return option.name;
-        }
-
-        /**
-         * Checks whether the user has permission to execute this slash command.
-         * @param {ApplicationCommand} command - The name of the command.
-         * @returns {Promise<boolean|string>} - Whether the user has permission to execute this command. If not, the missing default permission.
-         */
-        async function canRunCommand(command) {
-            const memberPerms = interaction.member.permissionsIn(interaction.channel);
-
-            const memberPermissions = interaction.member.permissions?.toArray() ?? [];
-            const requiredPermissions = command.defaultMemberPermissions?.toArray() ?? [];
-            if(memberPerms.has(PermissionFlagsBits.Administrator)) return true;
-
-            const perms =
-                await interaction.guild.commands.permissions.fetch({ command: command.id }).catch(() => null)
-                ?? await interaction.guild.commands.permissions.fetch({ command: client.user.id }).catch(() => null);
-            if(!perms) return requiredPermissions.find(permission => !memberPermissions.includes(permission)) ?? true;
-
-            let canRun = true;
-            const everyone = perms.find(e => e.type === ApplicationCommandPermissionType.User && e.id === interaction.guild.id)?.permission;
-            if(!everyone) canRun = false;
-
-            const channelOverride = perms.find(e => e.type === ApplicationCommandPermissionType.Channel && e.id === interaction.channel.id)?.permission;
-            if(channelOverride === false) return false;
-
-            const everyChannel = perms.find(e => e.type === ApplicationCommandPermissionType.Channel && BigInt(e.id) === BigInt(interaction.guild.id) - 1n)?.permission;
-            if(!channelOverride && everyChannel === false) return false;
-
-            const roleOverrides = perms.filter(e => e.type === ApplicationCommandPermissionType.Role && e.id !== interaction.guildId && interaction.member.roles.cache.has(e.id));
-            if((channelOverride || channelOverride === undefined) && roleOverrides.some(e => e.permission)) canRun = true;
-            else if(roleOverrides.length && roleOverrides.every(e => !e.permission)) canRun = false;
-
-            const userOverride = perms.find(e => e.type === ApplicationCommandPermissionType.User && e.id === interaction.member.id)?.permission;
-
-            if(userOverride) return true;
-            else if(userOverride === false) return false;
-
-            return canRun;
-        }
     }
 }
