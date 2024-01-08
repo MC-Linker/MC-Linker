cc8bbf88637985894f255eba9f459d6b15d34ca2
Lianecx <84291452+Lianecx@users.noreply.github.com>
2024-01-08 13:44:54 +0100

    Added convert script
    Signed-off-by: Lianecx <84291452+Lianecx@users.noreply.github.com>

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,47 +1,24 @@
-import fs from 'fs-extra';
-
+import { Mongoose } from 'mongoose';
 
 /**
- * Migrates old server- and user data to the mongodb database
- * @param {MCLinker} client - The client instance
+ * Convert a mongoose model to the new schema
+ * @param {Mongoose} mongoose
  */
-export async function migrate(client) {
-    const serverFolders = fs.readdirSync('./serverdata/connections');
-    for (const folder of serverFolders) {
-        if (!fs.existsSync(`./serverdata/connections/${folder}/connection.json`)) {
-            console.log(`No connection.json found in ${folder}`);
-            continue;
-        }
-        const server = JSON.parse(fs.readFileSync(`./serverdata/connections/${folder}/connection.json`, 'utf8'));
-        if (typeof server.version === 'string') server.version = parseInt(server.version.split('.')[1] ?? 19);
-        await client.serverConnections.connect(server);
-        console.log(`Connected server ${server.id}`);
+export function convert(mongoose) {
+    const serverConnectionModel = mongoose.models.ServerConnection;
 
-        if (!fs.existsSync(`./serverdata/connections/${folder}/settings.json`)) {
-            console.log(`No settings.json found in ${folder}`);
-            continue;
+    // Convert all requiredRoleToJoin fields to arrays
+    serverConnectionModel.find({ requiredRoleToJoin: { $type: 2 } }, (err, docs) => {
+        if(err) {
+            console.error(err);
+            return;
         }
-        const serverSettings = JSON.parse(fs.readFileSync(`./serverdata/connections/${folder}/settings.json`, 'utf8'));
-        await client.serverSettingsConnections.connect(serverSettings);
-        console.log(`Connected server settings ${server.id}`);
-    }
 
-    const userFolders = fs.readdirSync('./userdata/connections');
-    for (const folder of userFolders) {
-        if (!fs.existsSync(`./userdata/connections/${folder}/connection.json`)) {
-            console.log(`No connection.json found in ${folder}`);
-            continue;
-        }
-        const user = JSON.parse(fs.readFileSync(`./userdata/connections/${folder}/connection.json`, 'utf8'));
-        await client.userConnections.connect(user);
-        console.log(`Connected user ${user.id}`);
-
-        if (!fs.existsSync(`./userdata/connections/${folder}/settings.json`)) {
-            console.log(`No settings.json found in ${folder}`);
-            continue;
-        }
-        const userSettings = JSON.parse(fs.readFileSync(`./userdata/connections/${folder}/settings.json`, 'utf8'));
-        await client.userSettingsConnections.connect(userSettings);
-        console.log(`Connected user settings ${user.id}`);
-    }
+        docs.forEach(doc => {
+            console.log(`Converting ${doc.id}...`);
+            doc.requiredRoleToJoin = { method: 'any', roles: [doc.requiredRoleToJoin] };
+            doc.save();
+            console.log(`Converted ${doc.id}!`);
+        });
+    });
 }
