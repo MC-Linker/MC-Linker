c5bd810c31935b358a40beded4e32e50449fc98e
Lianecx <emilian.klefe@web.de>
2024-03-26 18:06:55 +0100

    Fix: drawMinecraftNumber now draws directly on the specified canvas ctx

--- utilities/utils.js
+++ utilities/utils.js
@@ -17,7 +17,7 @@ import advancementData from '../resources/data/advancements.json' assert { type:
 import customStats from '../resources/data/stats_custom.json' assert { type: 'json' };
 import nbt from 'prismarine-nbt';
 import { ph } from './messages.js';
-import { Canvas, loadImage } from 'skia-canvas';
+import { loadImage } from 'skia-canvas';
 import emoji from 'emojione';
 import mojangson from 'mojangson';
 import { Authflow } from 'prismarine-auth';
@@ -635,35 +635,34 @@ const mcNumbers = await loadImage('./resources/images/misc/numbers.png');
 
 /**
  * Draws a minecraft number on a canvas.
- * @param {import('skia-canvas').CanvasRenderingContext2D} context - The canvas context to draw on.
+ * @param {import('skia-canvas').CanvasRenderingContext2D} ctx - The canvas context to draw on.
  * @param {int|string} num - The number to draw.
  * @param {int} x - The x position to start drawing at.
  * @param {int} y - The y position to start drawing at.
  * @param {int} width - The width of the number.
  * @param {int} height - The height of the number.
  */
-export async function drawMinecraftNumber(context, num, x = 0, y = 0, width, height) {
+export async function drawMinecraftNumber(ctx, num, x = 0, y = 0, width, height) {
+    ctx.save();
+
     num = num.toString();
 
-    const canvas = new Canvas(12 * num.length);
-    const ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = false;
 
     //Draw shadow
     for(let i = 0; i < num.length; i++)
-        ctx.drawImage(mcNumbers, num[i] * 5, 0, 5, 7, i * 12 + 2, 2, width, height);
+        ctx.drawImage(mcNumbers, num[i] * 5, 0, 5, 7, i * width + 2, 2, width, height);
 
     ctx.globalCompositeOperation = 'source-in';
     ctx.fillStyle = '#3E3E3E';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(x, y, width * num.length, height);
 
-    ctx.globalCompositeOperation = 'source-over';
     //Draw number
+    ctx.globalCompositeOperation = 'source-over';
     for(let i = 0; i < num.length; i++)
-        ctx.drawImage(mcNumbers, num[i] * 5, 0, 5, 7, i * 12, 0, width, height);
+        ctx.drawImage(mcNumbers, num[i] * 5, 0, 5, 7, i * width, 0, width, height);
 
-    const image = await loadImage(canvas.toBufferSync('png'));
-    context.drawImage(image, x, y);
+    ctx.restore();
 }
 
 /**
