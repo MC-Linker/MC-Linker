7dd6502be92f4fd120786baa032e7cc44caf0200
Lianecx <84291452+Lianecx@users.noreply.github.com>
2024-07-22 19:16:29 +0200

    feat: Added storage statistics to ^systemstats
    Signed-off-by: Lianecx <84291452+Lianecx@users.noreply.github.com>

--- commands/SystemStats.js
+++ commands/SystemStats.js
@@ -4,6 +4,7 @@ import os from 'node:os';
 import { getReplyOptions, ph } from '../utilities/messages.js';
 import Discord from 'discord.js';
 import { durationString } from '../utilities/utils.js';
+import fs from 'fs-extra';
 
 export default class SystemStats extends Command {
 
@@ -29,16 +30,28 @@ export default class SystemStats extends Command {
 
         const memoryUsage = ((os.totalmem() - os.freemem()) / this.gigabyte).toFixed(2);
         const maxMemory = (os.totalmem() / this.gigabyte).toFixed(2);
+        const storage = await new Promise((resolve, _) => fs.statfs(process.platform === 'win32' ? 'C:/' : '/', (err, stats) => {
+            if(err) return resolve(null);
+            return resolve({
+                used: stats.blocks * stats.bsize - stats.bfree * stats.bsize,
+                max: stats.blocks * stats.bsize,
+            });
+        }));
+
         return await stats.edit(getReplyOptions(keys.commands.systemstats.success, {
             platform: `${os.platform()} ${os.release()}`,
             os_uptime: `${durationString(os.uptime() * 1000)}`,
             bot_uptime: `${durationString(client.uptime)}`,
             cpu: `${os.cpus()[0].model}`,
-            cpu_usage_percent: `${(await this.getCPUUsage() * 100).toFixed(2)}%`,
+            cpu_usage_percent: (await this.getCPUUsage() * 100).toFixed(2),
             memory_usage: memoryUsage,
             max_memory: maxMemory,
-            memory_usage_percent: `${(memoryUsage / maxMemory * 100).toFixed(2)}%`,
+            memory_usage_percent: (memoryUsage / maxMemory * 100).toFixed(2),
             library: `discord.js ${Discord.version}`,
+            node_version: process.version,
+            storage_usage: `${storage?.used ? (storage.used / this.gigabyte).toFixed(2) : 'N/A'}`,
+            max_storage: `${storage?.max ? (storage.max / this.gigabyte).toFixed(2) : 'N/A'}`,
+            storage_usage_percent: `${storage?.used ? (storage.used / storage.max * 100).toFixed(2) : 'N/A'}`,
         }, ph.colors()));
     }
 
