6265e89c627094e2948f1e743c828c0f976b59cf
Lianecx <emilian.klefe@web.de>
2025-07-21 00:05:41 +0200

    Fix pino transports
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- utilities/logger.js
+++ utilities/logger.js
@@ -2,14 +2,23 @@ import pino from 'pino';
 
 const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: process.env.NODE_ENV === 'production' ? undefined : {
-        target: 'pino-pretty',
-        options: {
-            colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname',
+    transport: [
+        process.env.NODE_ENV === 'production' ? undefined : {
+            target: 'pino-pretty',
+            options: {
+                colorize: true,
+                translateTime: 'SYS:standard',
+                ignore: 'pid,hostname',
+            },
         },
-    },
-}, pino.destination(`./logs/'${new Date().toISOString().split('T')[0]}.log`));
+        {
+            target: 'pino/file',
+            options: {
+                destination: `./logs/${new Date().toISOString().split('T')[0]}.log`,
+                mkdir: true,
+            },
+        },
+    ],
+});
 
 export default logger; 
