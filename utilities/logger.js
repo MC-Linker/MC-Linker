8f39ee34c1e535f14e3d6a26dda7cecbe3462115
Lianecx <emilian.klefe@web.de>
2025-07-21 16:41:58 +0200

    Fix pino config
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- utilities/logger.js
+++ utilities/logger.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const transport = pino.transport({
     targets: [
-        process.env.NODE_ENV === 'production' ? undefined : {
+        {
             target: 'pino-pretty',
             options: {
                 colorize: true,
