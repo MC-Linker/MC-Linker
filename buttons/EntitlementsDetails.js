93c867392a263522f6d204ee439ea98ffaf29186
Lianecx <emilian.klefe@web.de>
2025-07-25 18:31:27 +0200

    Remove useless error catch
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- buttons/EntitlementsDetails.js
+++ buttons/EntitlementsDetails.js
@@ -17,130 +17,125 @@ export default class EntitlementsDetails extends Button {
         if(interaction.entitlements.size === 0 && process.env.NODE_ENV === 'production')
             return await interaction.update(getReplyOptions(keys.warnings.errors.no_entitlement));
 
+        //Send modal
+        await interaction.showModal(getModal(keys.entitlements.success.details_modal));
+        const modal = await interaction.awaitModalSubmit({ time: 300_000 });
+        const token = modal.fields.getTextInputValue('token');
+
+        await modal.deferUpdate();
+        console.log(token);
+        await interaction.replyTl(keys.entitlements.success.logging_in);
+
+        let invite;
+
+        const testClient = new Discord.Client({
+            intents: [
+                Discord.GatewayIntentBits.GuildMessages,
+                Discord.GatewayIntentBits.GuildMembers,
+            ],
+        });
         try {
-            //Send modal
-            await interaction.showModal(getModal(keys.entitlements.success.details_modal));
-            const modal = await interaction.awaitModalSubmit({ time: 300_000 });
-            const token = modal.fields.getTextInputValue('token');
-
-            await modal.deferUpdate();
-            console.log(token);
-            await interaction.replyTl(keys.entitlements.success.logging_in);
-
-            let invite;
-
-            const testClient = new Discord.Client({
-                intents: [
-                    Discord.GatewayIntentBits.GuildMessages,
-                    Discord.GatewayIntentBits.GuildMembers,
-                ],
+            await testClient.login(token);
+            invite = testClient.generateInvite({
+                scopes: [],
+                permissions: [],
             });
-            try {
-                await testClient.login(token);
-                invite = testClient.generateInvite({
-                    scopes: [],
-                    permissions: [],
-                });
-            }
-            catch(err) {
-                console.log(err);
-                if(err.code === 'TokenInvalid')
-                    return await interaction.replyTl(keys.entitlements.warnings.invalid_token);
-                else if(err.message === 'Used disallowed intents')
-                    return await interaction.replyTl(keys.entitlements.warnings.no_intents);
-            }
-            finally {
-                await testClient.destroy();
-            }
-
-            //For linked roles they'll have to add endpoints in the portal and provide the secret
-
-            const botFolder = `./Custom-MC-Linker/${interaction.user.id}`;
-            if(await fs.exists(botFolder)) console.log(execSync('git pull', { cwd: botFolder }).toString());
-            else {
-                // Clone MC-Linker to ../../Custom-MC-Linker/<author_id>
-                await interaction.replyTl(keys.entitlements.success.cloning);
-                //TODO remove branch dev
-                logger.info(execSync(`git clone https://github.com/MC-Linker/MC-Linker --branch dev ${botFolder}`).toString());
-                // Copy docker-compose.yml
-                await fs.copy('./docker-compose-custom.yml', `${botFolder}/docker-compose.yml`);
-                await fs.mkdir(`${botFolder}/download-cache`);
-            }
-
-            const botPort = 30000; //TODO
-            const env = {
-                BOT_PORT: botPort,
-                PLUGIN_PORT: process.env.PLUGIN_PORT,
-                CLIENT_ID: testClient.user.id,
-                CLIENT_SECRET: '',
-                TOKEN: token,
-                COOKIE_SECRET: crypto.randomUUID(),
-                DISCORD_LINK: process.env.DISCORD_LINK,
-                GUILD_ID: `\'${process.env.GUILD_ID}\'`,
-                OWNER_ID: process.env.OWNER_ID,
-                PLUGIN_VERSION: process.env.PLUGIN_VERSION,
-                PREFIX: process.env.PREFIX,
-                LINKED_ROLES_REDIRECT_URI: `http://api.mclinker.com:${botPort}/linked-role/callback`,
-                MICROSOFT_EMAIL: process.env.MICROSOFT_EMAIL,
-                MICROSOFT_PASSWORD: `\"${process.env.MICROSOFT_PASSWORD}\"`,
-                AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
-                SERVICE_NAME: `custom-mc-linker_${interaction.user.id}`,
-                DATABASE_URL: `mongodb://mongodb:27017/custom-mc-linker_${interaction.user.id}`,
-                NODE_ENV: 'production',
-            };
-
-            const stringifiedEnv = Object.entries(env).map(([key, value]) => `${key}=${value}`).join('\n');
-            await fs.writeFile(`${botFolder}/.env`, stringifiedEnv);
-
-            await interaction.replyTl(keys.entitlements.success.starting_up);
-            logger.info(execSync(`docker build . -t lianecx/${env.SERVICE_NAME}`, { cwd: botFolder, env }).toString());
-
-            const composeProcess = spawn('docker', ['compose', 'up'], {
-                cwd: botFolder,
-                env: { ...env },
-                stdio: 'pipe',
-                timeout: 30_000, // 30 seconds
-            });
-
-            await new Promise((resolve, reject) => {
-                let output = '';
-
-                composeProcess.stdout.on('data', data => {
-                    const chunk = data.toString();
-                    output += chunk;
-                    logger.info(chunk);
-
-                    if(chunk.includes(`Server listening at http://0.0.0.0:${botPort}`)) {
-                        // Detach process from docker (Ctrl+P, Ctrl+Q)
-                        composeProcess.stdin.write(Buffer.from([0x10, 0x11])); // Ctrl+P, Ctrl+Q
-                        composeProcess.kill();
-                        resolve();
-                    }
-                });
-
-                composeProcess.stderr.on('data', data => {
-                    logger.error(data.toString());
-                });
-
-                composeProcess.on('error', reject);
-            });
-
-            //TODO Check for errors (wrong secret etc)
-            logger.info(execSync(`docker logs ${env.SERVICE_NAME}`).toString());
-
-            await interaction.replyTl(keys.entitlements.success.deploying);
-            logger.info(execSync(`docker exec ${env.SERVICE_NAME} node scripts/deploy.js deploy -g`, {
-                cwd: botFolder,
-                env,
-            }).toString());
-
-            // TODO
-            await exposeCustomBotPorts(botPort, botPort);
-
-            return await interaction.replyTl(keys.entitlements.success.port, { port: botPort, invite });
         }
-        catch(e) {
-            console.error(e);
+        catch(err) {
+            console.log(err);
+            if(err.code === 'TokenInvalid')
+                return await interaction.replyTl(keys.entitlements.warnings.invalid_token);
+            else if(err.message === 'Used disallowed intents')
+                return await interaction.replyTl(keys.entitlements.warnings.no_intents);
         }
+        finally {
+            await testClient.destroy();
+        }
+
+        //For linked roles they'll have to add endpoints in the portal and provide the secret
+
+        const botFolder = `./Custom-MC-Linker/${interaction.user.id}`;
+        if(await fs.exists(botFolder)) console.log(execSync('git pull', { cwd: botFolder }).toString());
+        else {
+            // Clone MC-Linker to ../../Custom-MC-Linker/<author_id>
+            await interaction.replyTl(keys.entitlements.success.cloning);
+            //TODO remove branch dev
+            logger.info(execSync(`git clone https://github.com/MC-Linker/MC-Linker --branch dev ${botFolder}`).toString());
+            // Copy docker-compose.yml
+            await fs.copy('./docker-compose-custom.yml', `${botFolder}/docker-compose.yml`);
+            await fs.mkdir(`${botFolder}/download-cache`);
+        }
+
+        const botPort = 30000; //TODO
+        const env = {
+            BOT_PORT: botPort,
+            PLUGIN_PORT: process.env.PLUGIN_PORT,
+            CLIENT_ID: testClient.user.id,
+            CLIENT_SECRET: '',
+            TOKEN: token,
+            COOKIE_SECRET: crypto.randomUUID(),
+            DISCORD_LINK: process.env.DISCORD_LINK,
+            GUILD_ID: `\'${process.env.GUILD_ID}\'`,
+            OWNER_ID: process.env.OWNER_ID,
+            PLUGIN_VERSION: process.env.PLUGIN_VERSION,
+            PREFIX: process.env.PREFIX,
+            LINKED_ROLES_REDIRECT_URI: `http://api.mclinker.com:${botPort}/linked-role/callback`,
+            MICROSOFT_EMAIL: process.env.MICROSOFT_EMAIL,
+            MICROSOFT_PASSWORD: `\"${process.env.MICROSOFT_PASSWORD}\"`,
+            AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
+            SERVICE_NAME: `custom-mc-linker_${interaction.user.id}`,
+            DATABASE_URL: `mongodb://mongodb:27017/custom-mc-linker_${interaction.user.id}`,
+            NODE_ENV: 'production',
+        };
+
+        const stringifiedEnv = Object.entries(env).map(([key, value]) => `${key}=${value}`).join('\n');
+        await fs.writeFile(`${botFolder}/.env`, stringifiedEnv);
+
+        await interaction.replyTl(keys.entitlements.success.starting_up);
+        logger.info(execSync(`docker build . -t lianecx/${env.SERVICE_NAME}`, { cwd: botFolder, env }).toString());
+
+        const composeProcess = spawn('docker', ['compose', 'up'], {
+            cwd: botFolder,
+            env: { ...env },
+            stdio: 'pipe',
+            timeout: 30_000, // 30 seconds
+        });
+
+        await new Promise((resolve, reject) => {
+            let output = '';
+
+            composeProcess.stdout.on('data', data => {
+                const chunk = data.toString();
+                output += chunk;
+                logger.info(chunk);
+
+                if(chunk.includes(`Server listening at http://0.0.0.0:${botPort}`)) {
+                    // Detach process from docker (Ctrl+P, Ctrl+Q)
+                    composeProcess.stdin.write(Buffer.from([0x10, 0x11])); // Ctrl+P, Ctrl+Q
+                    composeProcess.kill();
+                    resolve();
+                }
+            });
+
+            composeProcess.stderr.on('data', data => {
+                logger.error(data.toString());
+            });
+
+            composeProcess.on('error', reject);
+        });
+
+        //TODO Check for errors (wrong secret etc)
+        logger.info(execSync(`docker logs ${env.SERVICE_NAME}`).toString());
+
+        await interaction.replyTl(keys.entitlements.success.deploying);
+        logger.info(execSync(`docker exec ${env.SERVICE_NAME} node scripts/deploy.js deploy -g`, {
+            cwd: botFolder,
+            env,
+        }).toString());
+
+        // TODO
+        await exposeCustomBotPorts(botPort, botPort);
+
+        return await interaction.replyTl(keys.entitlements.success.port, { port: botPort, invite });
     }
 }
