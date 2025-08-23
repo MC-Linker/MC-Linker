805211453d0c406e328c045f8024b38fd667f95f
Lianecx <emilian.klefe@web.de>
2025-08-24 00:27:31 +0200

    fixed confirm delete modal replies
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- components/CustomizeConfirmDelete.js
+++ components/CustomizeConfirmDelete.js
@@ -2,7 +2,6 @@ import Component from '../structures/Component.js';
 import keys from '../utilities/keys.js';
 import logger from '../utilities/logger.js';
 import { getReplyOptions, ph } from '../utilities/messages.js';
-import { MessageFlags } from 'discord.js';
 
 export default class CustomizeTokenModal extends Component {
 
@@ -10,7 +9,7 @@ export default class CustomizeTokenModal extends Component {
         super({
             type: 'ModalSubmit',
             id: 'customize_confirm_delete',
-            defer: false,
+            defer: true,
             ephemeral: true,
             sku: '1166098447665995807',
         });
@@ -20,9 +19,7 @@ export default class CustomizeTokenModal extends Component {
         if(!await super.execute(interaction, client)) return;
 
         if(interaction.fields.getTextInputValue('confirm_delete') !== 'delete')
-            return await interaction.reply(getReplyOptions(keys.custom_bot.custom_bot_manager.warnings.invalid_confirmation, ph.emojisAndColors()));
-
-        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+            return await interaction.editReply(getReplyOptions(keys.custom_bot.custom_bot_manager.warnings.invalid_confirmation, ph.emojisAndColors()));
 
         const reason = interaction.fields.getTextInputValue('reason') || 'No reason provided';
         logger.info(`Custom bot connection for ${interaction.user.id} deleted with reason: ${reason}`);
