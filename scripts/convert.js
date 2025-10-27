0eba3feb37b3d13daa69e45af1392abd0617827b
Lianecx <emilian.klefe@web.de>
2025-10-27 01:36:39 +0100

    Uncomment HTTP protocol cleanup logic in `convert.js`
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,4 +1,6 @@
 import { Mongoose } from 'mongoose';
+import keys from '../utilities/keys.js';
+import { sendToServer } from '../utilities/utils.js';
 
 /**
  * Convert a mongoose model to the new schema
@@ -8,15 +10,14 @@ import { Mongoose } from 'mongoose';
 export async function convert(client, mongoose) {
     const serverConnectionModel = mongoose.models.ServerConnection;
 
-    // TODO Remove all http type server connections
-    /*    const docs = await serverConnectionModel.find({ protocol: 'http' }).exec();
-        console.log(`[${client.shard.ids[0]}] Found ${docs.length} server connections with protocol http.`);
-        for(const doc of docs) {
-            //Fetch guild
-            const guild = await client.guilds.fetch(doc._id);
-            await sendToServer(guild, keys.main.warnings.http_deprecated);
+    const docs = await serverConnectionModel.find({ protocol: 'http' }).exec();
+    console.log(`[${client.shard.ids[0]}] Found ${docs.length} server connections with protocol http.`);
+    for(const doc of docs) {
+        //Fetch guild
+        const guild = await client.guilds.fetch(doc._id);
+        await sendToServer(guild, keys.main.warnings.http_deprecated);
 
-            await serverConnectionModel.deleteOne({ _id: doc._id });
-            console.log(`[${client.shard.ids[0]}] Removed server connection with id ${doc._id} and protocol http.`);
-        }*/
+        await serverConnectionModel.deleteOne({ _id: doc._id });
+        console.log(`[${client.shard.ids[0]}] Removed server connection with id ${doc._id} and protocol http.`);
+    }
 }
