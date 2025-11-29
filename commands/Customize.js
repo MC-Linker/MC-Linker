90fbf43ac49249d14fb6f48bc56bca4c3ddbe588
Lianecx <emilian.klefe@web.de>
2025-11-29 13:31:32 +0100

    /customize now for customization of bot guild profile
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- commands/Customize.js
+++ commands/Customize.js
@@ -1,5 +1,7 @@
 import Command from '../structures/Command.js';
 import keys from '../utilities/keys.js';
+import { ComponentType, InteractionCollector, InteractionType, PermissionFlagsBits } from 'discord.js';
+import { addTranslatedResponses, getModal } from '../utilities/messages.js';
 
 export default class Customize extends Command {
 
@@ -10,15 +12,53 @@ export default class Customize extends Command {
             requiresConnectedServer: false,
             allowUser: true,
             ephemeral: true,
-            sku: '1166098447665995807',
         });
     }
 
     async execute(interaction, client, args, server) {
         if(!await super.execute(interaction, client, args, server)) return;
 
-        if(process.env.CUSTOM_BOT === 'true')
-            return await interaction.replyTl(keys.api.command.no_access.disabled);
+        if(!interaction.entitlements.has('1166098447665995807')) {
+            if(!interaction.inGuild()) return await interaction.replyTl(keys.commands.customize.warnings.no_entitlement_guild);
+
+            if(!interaction.memberPermissions.any([PermissionFlagsBits.Administrator, PermissionFlagsBits.ManageGuild])) {
+                return await interaction.replyTl(keys.main.no_access.no_permission_command, {
+                    permission: 'Administrator / Manage Server',
+                });
+            }
+            const message = await interaction.replyTl(keys.commands.customize.warnings.no_entitlement);
+
+            const buttonCollector = message.createMessageComponentCollector({
+                time: 60_000 * 14,
+                componentType: ComponentType.Button,
+            });
+            buttonCollector.on('collect', async buttonInteraction =>
+                await buttonInteraction.showModal(getModal(keys.commands.customize.customize_guild_appearance_modal)));
+
+            const modalCollector = new InteractionCollector(this.client, {
+                time: 60_000 * 14,
+                interactionType: InteractionType.ModalSubmit,
+                message,
+            });
+            modalCollector.on('collect', async modalInteraction => {
+                modalInteraction = addTranslatedResponses(modalInteraction);
+
+                const nick = modalInteraction.fields.getTextInputValue('nickname') ?? null;
+                const bio = modalInteraction.fields.getTextInputValue('bio') ?? null;
+                const avatar = modalInteraction.fields.getUploadedFiles('avatar').first() ?? null;
+                const banner = modalInteraction.fields.getUploadedFiles('banner').first() ?? null;
+
+                await interaction.guild.members.editMe({
+                    nick,
+                    avatar,
+                    banner,
+                    bio,
+                    reason: `Customized bot appearance by ${interaction.user.displayName} (${interaction.user.id})`,
+                });
+                await modalInteraction.replyTl(keys.commands.customize.success.guild_appearance_updated);
+            });
+            return;
+        }
 
         if(client.customBots.hasCustomBot(interaction.user.id))
             await client.customBots.sendCustomBotManager(interaction, client.customBots.getCustomBot(interaction.user.id));
