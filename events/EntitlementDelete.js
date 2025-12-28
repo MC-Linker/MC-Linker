087f4e13b1dac7fed47e708593a17b8e1858149b
Lianecx <emilian.klefe@web.de>
2025-12-28 22:52:33 +0100

    add todo
    Signed-off-by: Lianecx <emilian.klefe@web.de>

--- events/EntitlementDelete.js
+++ events/EntitlementDelete.js
@@ -14,6 +14,6 @@ export default class EntitlementDelete extends Event {
     }
 
     async execute(client, entitlement) {
-        // No logic implemented yet
+        // TODO No logic implemented yet
     }
 } 
