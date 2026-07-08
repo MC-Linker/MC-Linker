64bf5918dde4e103f9507b2bdab5fb7c5def6834
Lianecx <84291452+lianecx@users.noreply.github.com>
2026-07-08 20:51:14 +0200

    Reclassify ws_timeout and handle null server
    Signed-off-by: Lianecx <84291452+lianecx@users.noreply.github.com>

--- structures/protocol/WebSocketProtocol.js
+++ structures/protocol/WebSocketProtocol.js
@@ -282,12 +282,13 @@ export default class WebSocketProtocol extends Protocol {
             }, { track: false });
             return await new Promise(resolve => {
                 /** @type {WebSocketProtocol} */
-                const protocol = c.serverConnections.cache.get(id).protocol;
-                if(!protocol.socket) return resolve(null);
+                const protocol = c.serverConnections.cache.get(id)?.protocol;
+                if(!protocol?.socket) return resolve(null);
                 clog.debug(`Sending event ${name} with data: ${JSON.stringify(data)}`);
                 protocol.socket.timeout(10_000).emit(name, ...data, (err, response) => {
                     if(err) {
-                        c.analytics.trackError('unhandled', 'WebSocketProtocol._sendRaw', id, null, err, { event: name }, clog);
+                        const type = err?.message === 'operation has timed out' ? 'ws_timeout' : 'unhandled';
+                        c.analytics.trackError(type, 'WebSocketProtocol._sendRaw', id, null, err, { event: name }, clog);
                         return resolve(null);
                     }
 
